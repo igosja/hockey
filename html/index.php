b82da70926c47bd8da0fc4a48d1dd72b211f7a92
igosja <igosja@ukr.net>
2016-08-31 20:23:55 +0300

    init

---
+++ html/index.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <title>Hockey online manager</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+<div class="main">
+    <div class="content">
+        Logo<br/>
+        <form action="login">
+            <label for="top-form-login">Логин</label>
+            <input type="text" id="top-form-login" name="data[login]"/>
+            <label for="top-form-password">Пароль</label>
+            <input type="password" id="top-form-password" name="data[password]"/>
+            <input type="submit" value="Вход"/>
+        </form>
+        <div class="menu">
+            <a>Главная</a> | Новости | Онлайн | Правила | Опросы | Аллея славы | Регистрация в игре | Забыли
+            пароль?<br/>
+            Расписание | Турниры | Команды | Игроки | Трансфер | Обмены | Аренда<br/>
+            Форум | Чат | Ссылки | Контакты<br/>
+        </div>
+        Добро пожаловать в мир виртуального футбола!<br/>
+        Последние игровые новости<br/>
+        Как стать менеджером футбольной команды?<br/>
+        Футбольная аналитика<br/>
+        Новости федераций<br/>
+        Дни рождения
+    </div>
+</div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+if (isset($_GET['route'])) {
+    $file = $_GET['route'];
+} else {
+    $file = 'index';
+}
+
+include(__DIR__ . '/php/' . $file . '.php');
+include(__DIR__ . '/html/' . $file . '.php');
