f2295f2bfd6c361145aecb95c9a06ed4b8602ccc
Boyko Igor <igosja@ukr.net>
2016-10-17 16:55:32 +0300

    user questionarry

--- html/user/questionnaire.php
+++ html/user/questionnaire.php
@@ -46,9 +46,8 @@
         <div class="row text-size-4"><?= SPACE; ?></div>
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                День рождения: <span class="strong">
-                    <?= f_igosja_birth_date($user_array[0]); ?>
-                </span>
+                День рождения:
+                <span class="strong"><?= f_igosja_birth_date($user_array[0]); ?></span>
             </div>
         </div>
         <div class="row">
@@ -63,7 +62,8 @@
         </div>
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                Дата регистрации: <span class="strong"><?= f_igosja_ufu_date($user_array[0]['user_date_register']); ?></span>
+                Дата регистрации:
+                <span class="strong"><?= f_igosja_ufu_date($user_array[0]['user_date_register']); ?></span>
             </div>
         </div>
     </div>
@@ -78,4 +78,173 @@
         <?php include(__DIR__ . '/table_link.php'); ?>
     </div>
 </div>
-<>
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 table-responsive">
+        <table class="table">
+            <tr>
+                <th>Изменение анкетных данных менеджера</th>
+            </tr>
+        </table>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+        На этой странице вы можете <span class="strong">изменить свои анкетные данные</span>:
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 text-right">
+        <label for="questionnaire-name">Имя</label>:
+    </div>
+    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5 text-left">
+        <input
+            class="form-control form-small"
+            id="questionnaire-name"
+            name="data[user_name]"
+            type="text"
+            value="<?= $user_array[0]['user_name']; ?>"
+        />
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 text-right">
+        <label for="questionnaire-surname">Фамилия</label>:
+    </div>
+    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5 text-left">
+        <input
+            class="form-control form-small"
+            id="questionnaire-surname"
+            name="data[user_surname]"
+            type="text"
+            value="<?= $user_array[0]['user_surname']; ?>"
+        />
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 text-right">
+        <label for="questionnaire-email">Email</label>:
+    </div>
+    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5 text-left">
+        <input
+            class="form-control form-small"
+            id="questionnaire-email"
+            name="data[user_email]"
+            type="text"
+            value="<?= $user_array[0]['user_email']; ?>"
+        />
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 text-right">
+        <label for="questionnaire-city">Город</label>:
+    </div>
+    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5 text-left">
+        <input
+            class="form-control form-small"
+            id="questionnaire-city"
+            name="data[name]"
+            type="text"
+            value="<?= $user_array[0]['user_city']; ?>"
+        />
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 text-right">
+        <label for="questionnaire-country">Страна</label>:
+    </div>
+    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5 text-left">
+        <select class="form-control form-small" id="questionnaire-country" name="data[name]">
+            <option value="0">Не указано</option>
+            <?php foreach ($country_array as $item) { ?>
+                <option
+                    <?php if ($item['country_id'] == $user_array[0]['user_country_id']) { ?>
+                        selected
+                    <?php } ?>
+                    value="<?= $item['country_id']; ?>"
+                >
+                    <?= $item['country_name']; ?>
+                </option>
+            <?php } ?>
+        </select>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 text-right">
+        <label for="questionnaire-sex">Пол</label>:
+    </div>
+    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5 text-left">
+        <select class="form-control form-small" id="questionnaire-sex" name="data[name]">
+            <option value="0">Не указано</option>
+            <?php foreach ($sex_array as $item) { ?>
+                <option
+                    <?php if ($item['sex_id'] == $user_array[0]['user_sex_id']) { ?>
+                        selected
+                    <?php } ?>
+                    value="<?= $item['sex_id']; ?>"
+                >
+                    <?= $item['sex_name']; ?>
+                </option>
+            <?php } ?>
+        </select>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 text-right">
+        <label for="questionnaire-birth">Дата рождения</label>:
+    </div>
+    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5 text-left">
+        <select class="form-control form-small" id="questionnaire-birth" name="data[name]">
+            <option value="0">Не указано</option>
+            <?php for ($i=1; $i<=31; $i++) { ?>
+                <option
+                    <?php if ($i == $user_array[0]['user_birth_day']) { ?>
+                        selected
+                    <?php } ?>
+                    value="<?= $i; ?>"
+                >
+                    <?= $i; ?>
+                </option>
+            <?php } ?>
+        </select>
+        <select class="form-control form-small" name="data[name]">
+            <option value="0">Не указано</option>
+            <?php for ($i=1; $i<=12; $i++) { ?>
+                <option
+                    <?php if ($i == $user_array[0]['user_birth_day']) { ?>
+                        selected
+                    <?php } ?>
+                    value="<?= $i; ?>"
+                >
+                    <?= $i; ?>
+                </option>
+            <?php } ?>
+        </select>
+        <select class="form-control form-small" name="data[name]">
+            <option value="0">Не указано</option>
+            <?php for ($i=date('Y'); $i>=date('Y')-100; $i--) { ?>
+                <option
+                    <?php if ($i == $user_array[0]['user_birth_day']) { ?>
+                        selected
+                    <?php } ?>
+                    value="<?= $i; ?>"
+                >
+                    <?= $i; ?>
+                </option>
+            <?php } ?>
+        </select>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 text-right">
+        <label for="questionnaire-holiday">Я уехал в отпуск</label>:
+    </div>
+    <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5 text-left">
+        <input type="checkbox" id="questionnaire-holiday" name="data[name]"/> Назначить заместитетей
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center text-size-3">
+        Если вы поменяете свой e-mail, система автоматически отправит письмо на новый адрес с указанием,
+        как подтвердить, что ящик принадлежит вам и работает
+    </div>
+</div>
--- php/user/questionnaire.php
+++ php/user/questionnaire.php
@@ -8,11 +8,16 @@ $sql = "SELECT `country_name`,
                `user_birth_month`,
                `user_birth_year`,
                `user_city`,
+               `user_country_id`,
                `user_date_login`,
                `user_date_register`,
+               `user_email`,
                `user_finance`,
                `user_login`,
-               `user_money`
+               `user_money`,
+               `user_name`,
+               `user_sex_id`,
+               `user_surname`
         FROM `user`
         LEFT JOIN `sex`
         ON `user_sex_id`=`sex_id`
@@ -22,3 +27,19 @@ $sql = "SELECT `country_name`,
 $user_sql = igosja_db_query($sql);
 
 $user_array = $user_sql->fetch_all(1);
+
+$sql = "SELECT `country_id`,
+               `country_name`
+        FROM `country`
+        ORDER BY `country_name` ASC";
+$country_sql = igosja_db_query($sql);
+
+$country_array = $country_sql->fetch_all(1);
+
+$sql = "SELECT `sex_id`,
+               `sex_name`
+        FROM `sex`
+        ORDER BY `sex_id` ASC";
+$sex_sql = igosja_db_query($sql);
+
+$sex_array = $sex_sql->fetch_all(1);
