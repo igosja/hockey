bf9c4a2b9b97f653644a21ffb31268dc46f7a372
Boyko Igor <igosja@ukr.net>
2016-10-21 13:22:16 +0300

    move to yii1

--- html/include/admin/summary.php
+++
@@ -1,3 +0,0 @@
-<div class="summary">
-    Показаны записи <b><?= $summary_from; ?>-<?= $summary_to; ?></b> из <b><?= $count_item; ?></b>.
-</div>
--- js/admin.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function () {
-    $('#filters').find('input').on('change', function() {
-        $(this).parents('form').submit();
-    });
-    $('#filters').find('select').on('change', function() {
-        $(this).parents('form').submit();
-    });
-});
