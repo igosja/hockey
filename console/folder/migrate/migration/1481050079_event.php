b9fe1785520ffa1819eaf44b5aaaead35c4e4f18
Igor Boyko <igosja@ukr.net>
2016-12-06 21:51:00 +0200

    event table

---
+++ console/folder/migrate/migration/1481050079_event.php
@@ -0,0 +1,22 @@
+<?php
+
+$q = array();
+
+$q[] = 'CREATE TABLE `event`
+        (
+            `event_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
+            `event_eventtextgoal_id` TINYINT(1) DEFAULT 0,
+            `event_eventtextpenalty_id` TINYINT(2) DEFAULT 0,
+            `event_eventtype_id` TINYINT(1) DEFAULT 0,
+            `event_game_id` INT(11) DEFAULT 0,
+            `event_guest_score` TINYINT(2) DEFAULT 0,
+            `event_home_score` TINYINT(2) DEFAULT 0,
+            `event_minute` TINYINT(2) DEFAULT 0,
+            `event_national_id` SMALLINT(5) DEFAULT 0,
+            `event_player_assist_1_id` INT(11) DEFAULT 0,
+            `event_player_assist_2_id` INT(11) DEFAULT 0,
+            `event_player_id` INT(11) DEFAULT 0,
+            `event_player_penalty_id` INT(11) DEFAULT 0, #Против кого нарушили правила
+            `event_second` TINYINT(2) DEFAULT 0,
+            `event_team_id` SMALLINT(5) DEFAULT 0
+        );';
---
+++ console/folder/migrate/migration/1481051499_eventtextgoal.php
@@ -0,0 +1,17 @@
+<?php
+
+$q = array();
+
+$q[] = 'CREATE TABLE `eventtextgoal`
+        (
+            `eventtextgoal_id` TINYINT(1) PRIMARY KEY AUTO_INCREMENT,
+            `eventtextgoal_text` VARCHAR(255) NOT NULL
+        );';
+$q[] = "INSERT INTO `eventtextgoal` (`eventtextgoal_text`)
+        VALUES ('Выход 1 на 1'),
+               ('Щелчок'),
+               ('Мощный кистевой бросок'),
+               ('Быстрый кистевой бросок'),
+               ('Реализованный буллит'),
+               ('Нереализованный буллит'),
+               ('Гол в пустые ворота');";
