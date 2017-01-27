615a989abf948a239ffb2a6b095748319fbce76e
Igor Boyko <igosja@ukr.net>
2017-01-27 20:41:25 +0200

    user event

--- user_view.php
+++ user_view.php
@@ -38,4 +38,16 @@ if (0 == $user_sql->num_rows)
 
 $user_array = $user_sql->fetch_all(1);
 
+$sql = "SELECT `log_date`,
+               `log_season_id`,
+               `logtext_name`
+        FROM `log`
+        LEFT JOIN `logtext`
+        ON `log_logtext_id`=`logtext_id`
+        WHERE `log_user_id`=$num_get
+        ORDER BY `log_id` DESC";
+$event_sql = f_igosja_mysqli_query($sql);
+
+$event_array = $event_sql->fetch_all(1);
+
 include (__DIR__ . '/view/layout/main.php');
--- view/user_view.php
+++ view/user_view.php
@@ -9,3 +9,26 @@
         <?php include(__DIR__ . '/include/user_table_link.php'); ?>
     </div>
 </div>
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 table-responsive">
+        <table class="table table-bordered table-hover">
+            <tr>
+                <th class="col-1" title="Сезон">С</th>
+                <th class="col-15">Дата</th>
+                <th>Событие</th>
+            </tr>
+            <?php foreach ($event_array as $item) { ?>
+                <tr>
+                    <td class="text-center"><?= $item['log_season_id']; ?></td>
+                    <td class="text-center"><?= f_igosja_ufu_date($item['log_date']); ?></td>
+                    <td><?= $item['logtext_name']; ?></td>
+                </tr>
+            <?php } ?>
+            <tr>
+                <th title="Сезон">С</th>
+                <th>Дата</th>
+                <th>Событие</th>
+            </tr>
+        </table>
+    </div>
+</div>
