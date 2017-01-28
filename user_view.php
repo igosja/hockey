7f7b197d173c4a1273cf66ee9144f37e2f223ee6
Igor Boyko <igosja@ukr.net>
2017-01-28 12:08:51 +0200

    user rating

--- user_view.php
+++ user_view.php
@@ -38,6 +38,167 @@ if (0 == $user_sql->num_rows)
 
 $user_array = $user_sql->fetch_all(1);
 
+$sql = "SELECT `championship_place`,
+               `city_name`,
+               `conference_place`,
+               `country_id`,
+               `country_name`,
+               `division_id`,
+               `division_name`,
+               `team_id`,
+               `team_name`,
+               `team_power_vs`,
+               `team_price_total`
+        FROM `team`
+        LEFT JOIN `stadium`
+        ON `team_stadium_id`=`stadium_id`
+        LEFT JOIN `city`
+        ON `stadium_city_id`=`city_id`
+        LEFT JOIN `country`
+        ON `city_country_id`=`country_id`
+        LEFT JOIN
+        (
+            SELECT `championship_place`,
+                   `championship_team_id`,
+                   `division_id`,
+                   `division_name`
+            FROM `championship`
+            LEFT JOIN `division`
+            ON `championship_division_id`=`division_id`
+            WHERE `championship_team_id` IN
+            (
+                SELECT `team_id`
+                FROM `team`
+                WHERE `team_user_id`=$num_get
+            )
+            AND `championship_season_id`=$igosja_season_id
+        ) AS `t1`
+        ON `team_id`=`championship_team_id`
+        LEFT JOIN
+        (
+            SELECT `conference_place`,
+                   `conference_team_id`
+            FROM `conference`
+            WHERE `conference_team_id` IN
+            (
+                SELECT `team_id`
+                FROM `team`
+                WHERE `team_user_id`=$num_get
+            )
+            AND `conference_season_id`=$igosja_season_id
+        ) AS `t2`
+        ON `team_id`=`conference_team_id`
+        WHERE `team_user_id`=$num_get
+        ORDER BY `team_id` ASC";
+$team_sql = f_igosja_mysqli_query($sql);
+
+$team_array = $team_sql->fetch_all(1);
+
+$sql = "SELECT `userrating_auto`,
+               `userrating_collision_loose`,
+               `userrating_collision_win`,
+               `userrating_game`,
+               `userrating_loose`,
+               `userrating_loose_equal`,
+               `userrating_loose_strong`,
+               `userrating_loose_super`,
+               `userrating_loose_weak`,
+               `userrating_looseover`,
+               `userrating_looseover_equal`,
+               `userrating_looseover_strong`,
+               `userrating_looseover_weak`,
+               `userrating_rating`,
+               `userrating_vs_super`,
+               `userrating_vs_rest`,
+               `userrating_win`,
+               `userrating_win_equal`,
+               `userrating_win_strong`,
+               `userrating_win_super`,
+               `userrating_win_weak`,
+               `userrating_winover`,
+               `userrating_winover_equal`,
+               `userrating_winover_strong`,
+               `userrating_winover_weak`
+        FROM `userrating`
+        WHERE `userrating_user_id`=$num_get
+        AND `userrating_season_id`=0
+        LIMIT 1";
+$userrating_total_sql = f_igosja_mysqli_query($sql);
+
+if (0 == $userrating_total_sql->num_rows)
+{
+    $sql = "INSERT INTO `userrating`
+            SET `userrating_user_id`=$num_get";
+    f_igosja_mysqli_query($sql);
+
+    $sql = "SELECT `userrating_auto`,
+                   `userrating_collision_loose`,
+                   `userrating_collision_win`,
+                   `userrating_game`,
+                   `userrating_loose`,
+                   `userrating_loose_equal`,
+                   `userrating_loose_strong`,
+                   `userrating_loose_super`,
+                   `userrating_loose_weak`,
+                   `userrating_looseover`,
+                   `userrating_looseover_equal`,
+                   `userrating_looseover_strong`,
+                   `userrating_looseover_weak`,
+                   `userrating_rating`,
+                   `userrating_vs_super`,
+                   `userrating_vs_rest`,
+                   `userrating_win`,
+                   `userrating_win_equal`,
+                   `userrating_win_strong`,
+                   `userrating_win_super`,
+                   `userrating_win_weak`,
+                   `userrating_winover`,
+                   `userrating_winover_equal`,
+                   `userrating_winover_strong`,
+                   `userrating_winover_weak`
+            FROM `userrating`
+            WHERE `userrating_user_id`=$num_get
+            AND `userrating_season_id`=0
+            LIMIT 1";
+    $userrating_total_sql = f_igosja_mysqli_query($sql);
+}
+
+$userrating_total_array = $userrating_total_sql->fetch_all(1);
+
+$sql = "SELECT `userrating_auto`,
+               `userrating_collision_loose`,
+               `userrating_collision_win`,
+               `userrating_game`,
+               `userrating_loose`,
+               `userrating_loose_equal`,
+               `userrating_loose_strong`,
+               `userrating_loose_super`,
+               `userrating_loose_weak`,
+               `userrating_looseover`,
+               `userrating_looseover_equal`,
+               `userrating_looseover_strong`,
+               `userrating_looseover_weak`,
+               `userrating_rating`,
+               `userrating_season_id`,
+               `userrating_vs_super`,
+               `userrating_vs_rest`,
+               `userrating_win`,
+               `userrating_win_equal`,
+               `userrating_win_strong`,
+               `userrating_win_super`,
+               `userrating_win_weak`,
+               `userrating_winover`,
+               `userrating_winover_equal`,
+               `userrating_winover_strong`,
+               `userrating_winover_weak`
+        FROM `userrating`
+        WHERE `userrating_user_id`=$num_get
+        AND `userrating_season_id`!=0
+        ORDER BY `userrating_season_id` DESC";
+$userrating_sql = f_igosja_mysqli_query($sql);
+
+$userrating_array = $userrating_sql->fetch_all(1);
+
 $sql = "SELECT `log_date`,
                `log_season_id`,
                `logtext_name`
--- view/user_view.php
+++ view/user_view.php
@@ -13,6 +13,145 @@
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 table-responsive">
         <table class="table table-bordered table-hover">
             <tr>
+                <th>Команда</th>
+                <th class="col-20">Дивизион</th>
+                <th class="col-10" title="Рейтинг силы команды">Vs</th>
+                <th class="col-20">Стоимость</th>
+            </tr>
+            <?php foreach ($team_array as $item) { ?>
+                <tr>
+                    <td>
+                        <a href="team_view.php?num=<?= $item['team_id']; ?>">
+                            <?= $item['team_name']; ?>
+                            (<?= $item['city_name']; ?>, <?= $item['country_name']; ?>)
+                        </a>
+                    </td>
+                    <td class="text-center">
+                        <?php if ($item['division_id']) { ?>
+                            <a href="/championship.php?country_id=<?= $item['country_id']; ?>&division_id=<?= $item['division_id']; ?>">
+                                <?= $item['country_name']; ?>,
+                                <?= $item['division_name']; ?>,
+                                <?= $item['championship_place']; ?> место
+                            </a>
+                        <?php } else { ?>
+                            <a href="/conference_table.php">
+                                Конференция, <?= $item['conference_place']; ?> место
+                            </a>
+                        <?php } ?>
+                    </td>
+                    <td class="text-center">
+                        <?= $item['team_power_vs']; ?>
+                    </td>
+                    <td class="text-right">
+                        <?= f_igosja_money($item['team_price_total']); ?>
+                    </td>
+                </tr>
+            <?php } ?>
+            <tr>
+                <th>Команда</th>
+                <th>Дивизион</th>
+                <th title="Рейтинг силы команды">Vs</th>
+                <th>Стоимость</th>
+            </tr>
+        </table>
+    </div>
+</div>
+<div class="row margin-top">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 table-responsive">
+        <table class="table table-bordered table-hover">
+            <tr>
+                <th class="col-3" title="Сезон">C</th>
+                <th>Рейтинг</th>
+                <th class="col-3-5" title="Игры">И</th>
+                <th class="col-3-5" title="Победы">B</th>
+                <th class="col-3-5" title="Победы в овертайме/по буллитам">BО</th>
+                <th class="col-3-5" title="Поражения в овертайме/по буллитам/ничьи">ПО</th>
+                <th class="col-3-5" title="Поражения">П</th>
+                <th class="col-3-5" title="Выигранные коллизии">К+</th>
+                <th class="col-3-5" title="Проигранные коллизии">К-</th>
+                <th class="col-3-5" title="Победы у команд с супернастроем">ВС</th>
+                <th class="col-3-5" title="Победы у сильных соперников">В+</th>
+                <th class="col-3-5" title="Победы у равных соперников">В=</th>
+                <th class="col-3-5" title="Победы у слабых соперников">В-</th>
+                <th class="col-3-5" title="Победы в овертайме/по буллитам у сильных соперников">ВО+</th>
+                <th class="col-3-5" title="Победы в овертайме/по буллитам у равных соперников">ВО=</th>
+                <th class="col-3-5" title="Победы в овертайме/по буллитам у слабых соперников">ВО-</th>
+                <th class="col-3-5" title="Поражения в овертайме/по буллитам/ничьи сильным соперникам">ПО+</th>
+                <th class="col-3-5" title="Поражения в овертайме/по буллитам/ничьи равным соперникам">ПО=</th>
+                <th class="col-3-5" title="Поражения в овертайме/по буллитам/ничьи слабым соперникам">ПО-</th>
+                <th class="col-3-5" title="Поражения сильным соперникам">П+</th>
+                <th class="col-3-5" title="Поражения равным соперникам">П=</th>
+                <th class="col-3-5" title="Поражения слабым соперникам">П-</th>
+                <th class="col-3-5" title="Поражения супернастроем">ПС</th>
+                <th class="col-3-5" title="Автосоставы">А</th>
+                <th class="col-3-5" title="Игры против супернастроя">VС</th>
+                <th class="col-3-5" title="Игры против отдыха">VО</th>
+            </tr>
+            <?php foreach ($userrating_array as $item) { ?>
+                <tr>
+                    <td class="text-center"><?= $item['userrating_season_id']; ?></td>
+                    <td class="text-center"><?= $item['userrating_rating']; ?></td>
+                    <td class="text-center"><?= $item['userrating_game']; ?></td>
+                    <td class="text-center"><?= $item['userrating_win']; ?></td>
+                    <td class="text-center"><?= $item['userrating_winover']; ?></td>
+                    <td class="text-center"><?= $item['userrating_looseover']; ?></td>
+                    <td class="text-center"><?= $item['userrating_loose']; ?></td>
+                    <td class="text-center"><?= $item['userrating_collision_win']; ?></td>
+                    <td class="text-center"><?= $item['userrating_collision_loose']; ?></td>
+                    <td class="text-center"><?= $item['userrating_win_super']; ?></td>
+                    <td class="text-center"><?= $item['userrating_win_strong']; ?></td>
+                    <td class="text-center"><?= $item['userrating_win_equal']; ?></td>
+                    <td class="text-center"><?= $item['userrating_win_weak']; ?></td>
+                    <td class="text-center"><?= $item['userrating_winover_strong']; ?></td>
+                    <td class="text-center"><?= $item['userrating_winover_equal']; ?></td>
+                    <td class="text-center"><?= $item['userrating_winover_weak']; ?></td>
+                    <td class="text-center"><?= $item['userrating_looseover_strong']; ?></td>
+                    <td class="text-center"><?= $item['userrating_looseover_equal']; ?></td>
+                    <td class="text-center"><?= $item['userrating_looseover_weak']; ?></td>
+                    <td class="text-center"><?= $item['userrating_loose_strong']; ?></td>
+                    <td class="text-center"><?= $item['userrating_loose_equal']; ?></td>
+                    <td class="text-center"><?= $item['userrating_loose_weak']; ?></td>
+                    <td class="text-center"><?= $item['userrating_loose_super']; ?></td>
+                    <td class="text-center"><?= $item['userrating_auto']; ?></td>
+                    <td class="text-center"><?= $item['userrating_vs_super']; ?></td>
+                    <td class="text-center"><?= $item['userrating_vs_rest']; ?></td>
+                </tr>
+            <?php } ?>
+            <tr>
+                <th></th>
+                <th><?= $userrating_total_array[0]['userrating_rating']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_game']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_win']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_winover']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_looseover']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_loose']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_collision_win']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_collision_loose']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_win_super']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_win_strong']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_win_equal']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_win_weak']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_winover_strong']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_winover_equal']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_winover_weak']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_looseover_strong']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_looseover_equal']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_looseover_weak']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_loose_strong']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_loose_equal']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_loose_weak']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_loose_super']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_auto']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_vs_super']; ?></th>
+                <th><?= $userrating_total_array[0]['userrating_vs_rest']; ?></th>
+            </tr>
+        </table>
+    </div>
+</div>
+<div class="row margin-top">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 table-responsive">
+        <table class="table table-bordered table-hover">
+            <tr>
                 <th class="col-1" title="Сезон">С</th>
                 <th class="col-15">Дата</th>
                 <th>Событие</th>
