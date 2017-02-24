aedd351544e8ede9b7cd4f75b5d9a342660c814e
Boyko Igor <igosja@ukr.net>
2017-02-24 15:47:22 +0200

    country team list

--- country_team.php
+++ country_team.php
@@ -10,12 +10,19 @@ if (!$num_get = (int) f_igosja_request_get('num'))
 include (__DIR__ . '/include/sql/country_view.php');
 
 $sql = "SELECT `team_id`,
-               `team_name`
+               `team_name`,
+               `user_date_login`,
+               `user_id`,
+               `user_login`,
+               `user_name`,
+               `user_surname`
         FROM `team`
         LEFT JOIN `stadium`
         ON `team_stadium_id`=`stadium_id`
         LEFT JOIN `city`
         ON `stadium_city_id`=`city_id`
+        LEFT JOIN `user`
+        ON `team_user_id`=`user_id`
         WHERE `city_country_id`=$num_get
         ORDER BY `team_name` ASC";
 $team_sql = f_igosja_mysqli_query($sql);
--- view/country_team.php
+++ view/country_team.php
@@ -4,6 +4,8 @@
         <table class="table table-bordered table-hover">
             <tr>
                 <th>Команда</th>
+                <th class="col-40">Менеджер</th>
+                <th class="col-20 hidden-xs">Последний визит</th>
             </tr>
             <?php foreach ($team_array as $item) { ?>
                 <tr <?php if (isset ($auth_team_id) && $auth_team_id == $item['team_id']) { ?>class="current"<?php } ?>>
@@ -12,8 +14,24 @@
                             <?= $item['team_name']; ?>
                         </a>
                     </td>
+                    <td class="text-center">
+                        <a href="/user_view.php?num=<?= $item['user_id']; ?>">
+                            <?= $item['user_login']; ?>
+                            <span class="hidden-xs">
+                                <?php if ($item['user_name'] || $item['user_surname']) { ?>
+                                    (<?= $item['user_name']; ?> <?= $item['user_surname']; ?>)
+                                <?php } ?>
+                            </span>
+                        </a>
+                    </td>
+                    <td class="hidden-xs text-center"><?= f_igosja_ufu_last_visit($item['user_date_login']); ?></td>
                 </tr>
             <?php } ?>
+            <tr>
+                <th>Команда</th>
+                <th>Менеджер</th>
+                <th class="hidden-xs">Последний визит</th>
+            </tr>
         </table>
     </div>
 </div>
