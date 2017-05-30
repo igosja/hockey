c9cb8d2dd3fea670daf22b5b5548e484b20b1ca3
Igor Boyko <igosja@ukr.net>
2017-05-30 20:23:34 +0300

    google analitics to head

--- view/layout/main.php
+++ view/layout/main.php
@@ -16,6 +16,18 @@
     <meta name="google-site-verification" content="RBlpWHwlnGqvB36CLDYF58VqxN0bcz5W5JbxcX-PTeQ" />
     <link rel="icon" type="image/x-icon" href="/favicon.ico">
     <link rel="stylesheet" href="/css/style.css">
+    <?php if ('vhol.org' == $_SERVER['HTTP_HOST']) { ?>
+        <!--Google analitics-->
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+            ga('create', 'UA-90926144-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+        <!--/Google analitics-->
+    <?php } ?>
 </head>
 <body>
 <div class="main">
@@ -99,16 +111,6 @@
     <script src="/js/<?= $tpl; ?>.js"></script>
 <?php } ?>
 <?php if ('vhol.org' == $_SERVER['HTTP_HOST']) { ?>
-    <!--Google analitics-->
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-        ga('create', 'UA-90926144-1', 'auto');
-        ga('send', 'pageview');
-    </script>
-    <!--/Google analitics-->
     <!--LiveInternet counter-->
     <script type="text/javascript"><!--
         new Image().src = "//counter.yadro.ru/hit?r"+
