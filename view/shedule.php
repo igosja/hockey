35e8e85f77b93bdd717241e0cbed5c536f8826bf
Boyko Igor <igosja@ukr.net>
2017-06-19 10:41:02 +0300

    codeception

--- view/shedule.php
+++ view/shedule.php
@@ -1,3 +1,10 @@
+<?php
+
+/**
+ * @var $shedule_array array
+ */
+
+?>
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <h1>Рассписание</h1>
@@ -12,7 +19,7 @@
                 <th class="col-20">Стадия</th>
             </tr>
             <?php foreach ($shedule_array as $item) { ?>
-                <tr>
+                <tr<?php if (date('Y-m-d', $item['shedule_date']) == date('Y-m-d')) { ?> class="info"<?php } ?>>
                     <td class="text-center"><?= f_igosja_ufu_date_time($item['shedule_date']); ?></td>
                     <td class="text-center">
                         <a href="/game_list.php?num=<?= $item['shedule_id']; ?>">
