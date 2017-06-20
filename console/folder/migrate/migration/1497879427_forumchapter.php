6476ea91b36668a45d6324e89eb051956e1adec6
Boyko Igor <igosja@ukr.net>
2017-06-20 15:31:12 +0300

    forum transaction

--- console/folder/migrate/migration/1497879427_forumchapter.php
+++ console/folder/migrate/migration/1497879427_forumchapter.php
@@ -7,11 +7,13 @@ $q[] = 'CREATE TABLE `forumchapter`
             `forumchapter_id` INT(11) PRIMARY KEY AUTO_INCREMENT,
             `forumchapter_count_message` INT(11) DEFAULT 0,
             `forumchapter_count_theme` INT(11) DEFAULT 0,
+            `forumchapter_description` TEXT NOT NULL,
             `forumchapter_forumgroup_id` INT(11) DEFAULT 0,
             `forumchapter_last_date` INT(11) DEFAULT 0,
             `forumchapter_last_forummessage_id` INT(11) DEFAULT 0,
             `forumchapter_last_forumtheme_id` INT(11) DEFAULT 0,
             `forumchapter_last_user_id` INT(11) DEFAULT 0,
+            `forumchapter_name` VARCHAR(255) NOT NULL,
             `forumchapter_order` SMALLINT(3) DEFAULT 0,
             `forumchapter_user_id` INT(11) DEFAULT 0
         );';
