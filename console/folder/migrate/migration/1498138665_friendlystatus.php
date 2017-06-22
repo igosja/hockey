4afd76e1f81426d1baa1c901dccb5e2b5e3ce38d
Boyko Igor <igosja@ukr.net>
2017-06-22 16:54:01 +0300

    friendly migration

---
+++ console/folder/migrate/migration/1498138665_friendlystatus.php
@@ -0,0 +1,13 @@
+<?php
+
+$q = array();
+
+$q[] = 'CREATE TABLE `friendlystatus`
+        (
+            `friendlystatus_id` TINYINT(1) PRIMARY KEY AUTO_INCREMENT,
+            `friendlystatus_name` VARCHAR(255) NOT NULL
+        );';
+$q[] = "INSERT INTO `friendlystatus` (`friendlystatus_name`)
+        VALUES ('Я принимаю любое приглашение'),
+               ('Я сам выбираю соперников для моей команды'),
+               ('Я не хочу принимать приглашения');
