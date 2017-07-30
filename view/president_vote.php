b933f267b5e4f45fe4a9e5b089eeb7a704580eae
Igor Boyko <igosja@ukr.net>
2017-07-30 13:13:36 +0300

    president vote

---
+++ view/president_vote.php
@@ -0,0 +1,47 @@
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+                <h1>Выборы президента федерации</h1>
+            </div>
+        </div>
+        <div class="row margin-top">
+            <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 text-right">
+                <?= $electionpresident_array[0]['electionstatus_name']; ?>
+            </div>
+        </div>
+        <?php foreach ($electionpresident_array as $item) { ?>
+            <div class="row">
+                <div class="col-lg-9 col-md-9 col-sm-9 col-xs-9">
+                    <a href="/user_view.php?num=<?= $item['user_id']; ?>">
+                        <?= $item['user_login']; ?>
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-3 col-xs-3 text-right">
+                    <?= $item['count_answer']; ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    Дата регистрации:
+                    <?= f_igosja_ufu_date($item['user_date_register']); ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    Рейтинг менеджера:
+                    <?= $item['userrating_rating']; ?>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    Текст программы:
+                    <br/>
+                    <?= nl2br($item['electionpresidentapplication_text']); ?>
+                </div>
+            </div>
+        <?php } ?>
+    </div>
+</div>
