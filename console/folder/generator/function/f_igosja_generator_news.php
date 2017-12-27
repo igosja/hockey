3c9f7086a9a375c2c1da82125082e35f6ef9b134
Boyko Igor <igosja@ukr.net>
2017-12-27 16:45:06 +0200

    auto news

---
+++ console/folder/generator/function/f_igosja_generator_news.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Авто новини після генерації ігрового дня
+ */
+function f_igosja_generator_achievement()
+{
+    $sql = "SELECT `stage_name`,
+                   `schedule_tournamenttype_id`
+            FROM `schedule`
+            LEFT JOIN `stage`
+            ON `schedule_stage_id`=`stage_id`
+            WHERE FROM_UNIXTIME(`schedule_date`, '%Y-%m-%d')=CURDATE()";
+    $today_sql = f_igosja_mysqli_query($sql);
+
+    $today_array = $today_sql->fetch_all(MYSQLI_ASSOC);
+
+    $sql = "SELECT `stage_name`,
+                   `schedule_tournamenttype_id`
+            FROM `schedule`
+            LEFT JOIN `stage`
+            ON `schedule_stage_id`=`stage_id`
+            WHERE FROM_UNIXTIME(`schedule_date`+86400, '%Y-%m-%d')=CURDATE()";
+    $tomorrow_sql = f_igosja_mysqli_query($sql);
+
+    $tomorrow_array = $tomorrow_sql->fetch_all(MYSQLI_ASSOC);
+}
