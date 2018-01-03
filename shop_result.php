0d4c52eeeb198b3d3780425292c088766c577f5a
Boyko Igor <igosja@ukr.net>
2018-01-03 09:31:41 +0200

    shop_result from post to get

--- shop_result.php
+++ shop_result.php
@@ -11,24 +11,24 @@ else
     $ip = $_SERVER['REMOTE_ADDR'];
 }
 
-//if (!in_array($ip, array('136.243.38.147', '136.243.38.149', '136.243.38.150', '136.243.38.151', '136.243.38.189', '88.198.88.98')))
-//{
-//    die('HACKING ATTEMPT');
-//}
+if (!in_array($ip, array('136.243.38.147', '136.243.38.149', '136.243.38.150', '136.243.38.151', '136.243.38.189', '88.198.88.98')))
+{
+    die('HACKING ATTEMPT');
+}
 
-if (!isset($_POST['MERCHANT_ID']) ||
-    !isset($_POST['AMOUNT']) ||
-    !isset($_POST['MERCHANT_ORDER_ID']) ||
-    !isset($_POST['SIGN']))
+if (!isset($_REQUEST['MERCHANT_ID']) ||
+    !isset($_REQUEST['AMOUNT']) ||
+    !isset($_REQUEST['MERCHANT_ORDER_ID']) ||
+    !isset($_REQUEST['SIGN']))
 {
     die('WRONG POST');
 }
 
 $merchant_id    = 27937;
 $secret         = 'h8lzyqfr';
-$sign           = $_POST['SIGN'];
-$payment_id     = $_POST['MERCHANT_ORDER_ID'];
-$sum            = $_POST['AMOUNT'];
+$sign           = $_REQUEST['SIGN'];
+$payment_id     = $_REQUEST['MERCHANT_ORDER_ID'];
+$sum            = $_REQUEST['AMOUNT'];
 $sign_check     = md5($merchant_id . ':' . $_REQUEST['AMOUNT'] . ':' . $secret . ':' . $payment_id);
 
 if ($sign_check != $sign)
@@ -63,68 +63,68 @@ if (1 == $status)
 $user_id    = $payment_array[0]['payment_user_id'];
 $sum        = $payment_array[0]['payment_sum'];
 
-//$sql = "UPDATE `payment`
-//        SET `payment_status`=1
-//        WHERE `payment_id`=$payment_id
-//        LIMIT 1";
-//f_igosja_mysqli_query($sql);
-//
-//$sql = "SELECT `user_money`,
-//               `user_referrer_id`
-//        FROM `user`
-//        WHERE `user_id`=$user_id
-//        LIMIT 1";
-//$user_sql = f_igosja_mysqli_query($sql);
-//
-//$user_array = $user_sql->fetch_all(MYSQLI_ASSOC);
-//
-//$sql = "UPDATE `user`
-//        SET `user_money`=`user_money`+$sum
-//        WHERE `user_id`=$user_id
-//        LIMIT 1";
-//f_igosja_mysqli_query($sql);
+$sql = "UPDATE `payment`
+        SET `payment_status`=1
+        WHERE `payment_id`=$payment_id
+        LIMIT 1";
+f_igosja_mysqli_query($sql);
 
-//$money = array(
-//    'money_moneytext_id' => MONEYTEXT_INCOME_ADD_FUNDS,
-//    'money_user_id' => $user_id,
-//    'money_value' => -$sum,
-//    'money_value_after' => $user_array[0]['user_money'] - $sum,
-//    'money_value_before' => $user_array[0]['user_money'],
-//);
-//f_igosja_money($money);
+$sql = "SELECT `user_money`,
+               `user_referrer_id`
+        FROM `user`
+        WHERE `user_id`=$user_id
+        LIMIT 1";
+$user_sql = f_igosja_mysqli_query($sql);
 
-//if ($user_array[0]['user_referrer_id'])
-//{
-//    $referrer_id = $user_array[0]['user_referrer_id'];
-//
-//    $sum = $sum / 10;
-//
-//    $sql = "SELECT `user_money`,
-//                   `user_referrer_id`
-//            FROM `user`
-//            WHERE `user_id`=$referrer_id
-//            LIMIT 1";
-//    $user_sql = f_igosja_mysqli_query($sql);
-//
-//    if ($user_sql->num_rows)
-//    {
-//        $user_array = $user_sql->fetch_all(MYSQLI_ASSOC);
-//
-//        $sql = "UPDATE `user`
-//                SET `user_money`=`user_money`+$sum
-//                WHERE `user_id`=$referrer_id
-//                LIMIT 1";
-//        f_igosja_mysqli_query($sql);
-//
-//        $money = array(
-//            'money_moneytext_id' => MONEYTEXT_INCOME_REFERRAL,
-//            'money_user_id' => $referrer_id,
-//            'money_value' => -$sum,
-//            'money_value_after' => $user_array[0]['user_money'] - $sum,
-//            'money_value_before' => $user_array[0]['user_money'],
-//        );
-//        f_igosja_money($money);
-//    }
-//}
+$user_array = $user_sql->fetch_all(MYSQLI_ASSOC);
+
+$sql = "UPDATE `user`
+        SET `user_money`=`user_money`+$sum
+        WHERE `user_id`=$user_id
+        LIMIT 1";
+f_igosja_mysqli_query($sql);
+
+$money = array(
+    'money_moneytext_id' => MONEYTEXT_INCOME_ADD_FUNDS,
+    'money_user_id' => $user_id,
+    'money_value' => -$sum,
+    'money_value_after' => $user_array[0]['user_money'] - $sum,
+    'money_value_before' => $user_array[0]['user_money'],
+);
+f_igosja_money($money);
+
+if ($user_array[0]['user_referrer_id'])
+{
+    $referrer_id = $user_array[0]['user_referrer_id'];
+
+    $sum = $sum / 10;
+
+    $sql = "SELECT `user_money`,
+                   `user_referrer_id`
+            FROM `user`
+            WHERE `user_id`=$referrer_id
+            LIMIT 1";
+    $user_sql = f_igosja_mysqli_query($sql);
+
+    if ($user_sql->num_rows)
+    {
+        $user_array = $user_sql->fetch_all(MYSQLI_ASSOC);
+
+        $sql = "UPDATE `user`
+                SET `user_money`=`user_money`+$sum
+                WHERE `user_id`=$referrer_id
+                LIMIT 1";
+        f_igosja_mysqli_query($sql);
+
+        $money = array(
+            'money_moneytext_id' => MONEYTEXT_INCOME_REFERRAL,
+            'money_user_id' => $referrer_id,
+            'money_value' => -$sum,
+            'money_value_after' => $user_array[0]['user_money'] - $sum,
+            'money_value_before' => $user_array[0]['user_money'],
+        );
+        f_igosja_money($money);
+    }
+}
 
 die('YES');
