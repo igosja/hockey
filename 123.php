0286c5368c55565e658e4968256b7f7234e674ec
Boyko Igor <igosja@ukr.net>
2018-01-04 12:43:03 +0200

    visitor fix

--- 123.php
+++ 123.php
@@ -4,39 +4,34 @@ include(__DIR__ . '/include/include.php');
 
 $sql = "SELECT `team_id`
         FROM `team`
-        WHERE `team_id`!=0
-        ORDER BY `team_id` ASC";
+        WHERE `team_id`!=0";
 $team_sql = f_igosja_mysqli_query($sql);
 
 $team_array = $team_sql->fetch_all(MYSQLI_ASSOC);
 
-foreach ($team_array as $team)
+foreach ($team_array as $item)
 {
-    $team_id = $team['team_id'];
+    $team_id = $item['team_id'];
 
-    $sql = "SELECT `finance_id`,
-                   `finance_value`
-            FROM `finance`
-            WHERE `finance_team_id`=$team_id
-            ORDER BY `finance_id` ASC";
-    $finance_sql = f_igosja_mysqli_query($sql);
+    $sql = "SELECT IF(COUNT(`teamvisitor_visitor`)=0, 1, COUNT(`teamvisitor_visitor`)) AS `count`,
+                   IF(SUM(`teamvisitor_visitor`) IS NULL, 1, SUM(`teamvisitor_visitor`)) AS `visitor`
+            FROM
+            (
+                SELECT `teamvisitor_visitor`
+                FROM `teamvisitor`
+                WHERE `teamvisitor_team_id`=$team_id
+                ORDER BY `teamvisitor_id` DESC
+                LIMIT 5
+            ) AS `t1`";
+    $visitor_sql = f_igosja_mysqli_query($sql);
 
-    $finance_array = $finance_sql->fetch_all(MYSQLI_ASSOC);
+    $visitor_array = $visitor_sql->fetch_all(MYSQLI_ASSOC);
 
-    $finance_before = 1000000;
+    $visitor = $visitor_array[0]['visitor'] / $visitor_array[0]['count'] * 100;
 
-    foreach ($finance_array as $item)
-    {
-        $finance_id = $item['finance_id'];
-        $finance_after = $finance_before + $item['finance_value'];
-
-        $sql = "UPDATE `finance`
-                SET `finance_value_before`=$finance_before,
-                    `finance_value_after`=$finance_after
-                WHERE `finance_id`=$finance_id
-                LIMIT 1";
-        f_igosja_mysqli_query($sql);
-
-        $finance_before = $finance_after;
-    }
+    $sql = "UPDATE `team`
+            SET `team_visitor`=$visitor
+            WHERE `team_id`=$team_id
+            LIMIT 1";
+    f_igosja_mysqli_query($sql);
 }
