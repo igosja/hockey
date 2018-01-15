5e1073bd347d71707bc67b0a51d8d3a58d3b1c24
Igor Boyko <igosja@ukr.net>
2018-01-15 09:16:01 +0000

    f_igosja_generator_rent.php edited online with Bitbucket

--- console/folder/generator/function/f_igosja_generator_rent.php
+++ console/folder/generator/function/f_igosja_generator_rent.php
@@ -102,6 +102,7 @@ function f_igosja_generator_rent()
                         SELECT `schedule_id`
                         FROM `schedule`
                         WHERE FROM_UNIXTIME(`schedule_date`, '%Y-%m-%d')=CURDATE()
+                        LIMIT 1
                     )";
             f_igosja_mysqli_query($sql);
 
