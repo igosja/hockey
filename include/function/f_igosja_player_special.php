c926bf4039955da966a43e5e32bf8cdd75fc91ab
Igor Boyko <igosja@ukr.net>
2018-01-22 21:54:17 +0200

    error log fix

--- include/function/f_igosja_player_special.php
+++ include/function/f_igosja_player_special.php
@@ -12,7 +12,7 @@ function f_igosja_player_special($player_id, $playerspecial_array)
 
     foreach ($playerspecial_array as $item)
     {
-        if ($item['playerspecial_player_id'] == $player_id)
+        if (isset($item['playerspecial_player_id']) && $item['playerspecial_player_id'] == $player_id)
         {
             $return_array[] = '<span title="' . $item['special_name'] . '">' . $item['special_short'] . $item['playerspecial_level'] . '</span>';
         }
