a8b6c2ae93e768933a221495dbc9724edc4e9203
Igor Boyko <igor.boyko@hotelfriend.co>
2018-01-24 11:29:44 +0200

    adsence add

--- view/layout/main.php
+++ view/layout/main.php
@@ -98,7 +98,17 @@
         <?php if ((!isset($auth_user_id) || $auth_date_vip < time()) && 'vhol.org' == $_SERVER['HTTP_HOST']) { ?>
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center hidden">
-                    Реклама
+                    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    <!-- vhol footer -->
+                    <ins class="adsbygoogle"
+                         style="display:block"
+                         data-ad-client="ca-pub-9189986235139627"
+                         data-ad-slot="8459831805"
+                         data-ad-format="auto"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
+                    <!-- vhol footer -->
                 </div>
             </div>
         <?php } ?>
