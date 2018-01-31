6eeaf3761e7c6f6d270bc42053309b1883b6e817
Igor Boyko <igor.boyko@hotelfriend.co>
2018-01-31 15:11:39 +0200

    review preview

--- view/review_create.php
+++ view/review_create.php
@@ -21,48 +21,6 @@
         Сезон <?= $season_id; ?>, <?= $country_array[0]['division_name']; ?>, <?= $game_array[0]['stage_name']; ?>
     </div>
 </div>
-<div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 table-responsive">
-        <table class="table">
-            <?php foreach ($game_array as $item) { ?>
-                <tr>
-                    <td class="text-right col-45">
-                        <a href="/team_view.php?num=<?= $item['home_team_id']; ?>">
-                            <?= $item['home_team_name']; ?>
-                            <span class="hidden-xs">(<?= $item['home_city_name']; ?>)</span>
-                        </a>
-                        <?= f_igosja_game_auto($item['game_home_auto']); ?>
-                    </td>
-                    <td class="text-center col-10">
-                        <a href="/game_view.php?num=<?= $item['game_id']; ?>">
-                            <?= f_igosja_game_score($item['game_played'], $item['game_home_score'], $item['game_guest_score']); ?>
-                        </a>
-                    </td>
-                    <td>
-                        <a href="/team_view.php?num=<?= $item['guest_team_id']; ?>">
-                            <?= $item['guest_team_name']; ?>
-                            <span class="hidden-xs">(<?= $item['guest_city_name']; ?>)</span>
-                        </a>
-                        <?= f_igosja_game_auto($item['game_guest_auto']); ?>
-                    </td>
-                </tr>
-            <?php } ?>
-        </table>
-    </div>
-</div>
-<div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <p class="text-justify">
-            Текст обзора должен быть информативным, выдержанным в тоне публикаций в спортивных СМИ,
-            освещать все матчи игрового дня в дивизионе, не должен содержать бурного выражения личных эмоций,
-            ненормативной лексики и оскорблений в чей-либо адрес,
-            не должен являться ответом на другой обзор или высказывание участника игры.
-        </p>
-        <p class="text-justify">
-            Будьте внимательны! Обзор нельзя отредактировать после сохранения.
-        </p>
-    </div>
-</div>
 <form action="/review_create.php?<?= http_build_query(array_merge($_GET, array('edit' => 0))); ?>" method="POST">
     <?php if ($preview) { ?>
         <input name="data[title]" type="hidden" value="<?= $review_title; ?>" />
@@ -90,6 +48,48 @@
             </div>
         </div>
     <?php } else { ?>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 table-responsive">
+                <table class="table">
+                    <?php foreach ($game_array as $item) { ?>
+                        <tr>
+                            <td class="text-right col-45">
+                                <a href="/team_view.php?num=<?= $item['home_team_id']; ?>">
+                                    <?= $item['home_team_name']; ?>
+                                    <span class="hidden-xs">(<?= $item['home_city_name']; ?>)</span>
+                                </a>
+                                <?= f_igosja_game_auto($item['game_home_auto']); ?>
+                            </td>
+                            <td class="text-center col-10">
+                                <a href="/game_view.php?num=<?= $item['game_id']; ?>">
+                                    <?= f_igosja_game_score($item['game_played'], $item['game_home_score'], $item['game_guest_score']); ?>
+                                </a>
+                            </td>
+                            <td>
+                                <a href="/team_view.php?num=<?= $item['guest_team_id']; ?>">
+                                    <?= $item['guest_team_name']; ?>
+                                    <span class="hidden-xs">(<?= $item['guest_city_name']; ?>)</span>
+                                </a>
+                                <?= f_igosja_game_auto($item['game_guest_auto']); ?>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </table>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <p class="text-justify">
+                    Текст обзора должен быть информативным, выдержанным в тоне публикаций в спортивных СМИ,
+                    освещать все матчи игрового дня в дивизионе, не должен содержать бурного выражения личных эмоций,
+                    ненормативной лексики и оскорблений в чей-либо адрес,
+                    не должен являться ответом на другой обзор или высказывание участника игры.
+                </p>
+                <p class="text-justify">
+                    Будьте внимательны! Обзор нельзя отредактировать после сохранения.
+                </p>
+            </div>
+        </div>
         <input name="data[preview]" type="hidden" value="1" />
         <div class="row">
             <div class="col-lg-2 col-md-2 col-sm-3 col-xs-4 text-right">
