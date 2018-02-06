9c7df7732ffddec53253553df8f3f4a177f6247a
Igor Boyko <igosja@ukr.net>
2018-02-06 23:36:48 +0200

    generator shot and assist rand change

--- console/folder/generator/secondary/f_igosja_select_assist_2.php
+++ console/folder/generator/secondary/f_igosja_select_assist_2.php
@@ -21,8 +21,8 @@ function f_igosja_select_assist_2($game_result, $team)
         $game_result['assist_2'] = rand(POSITION_LW, POSITION_RW);
     }
 
-    $penalty_position           = f_igosja_penalty_position_array($game_result, $team);
-    $penalty_position[]         = $game_result['player'];
+    $penalty_position   = f_igosja_penalty_position_array($game_result, $team);
+    $penalty_position[] = $game_result['player'];
 
     if (in_array($game_result['assist_2'], $penalty_position) || $game_result['assist_2'] == $game_result['assist_1'])
     {
