b82b419f62cdb7dc7dd0a816271e5f695215d3e6
Igor Boyko <igor.boyko@hotelfriend.co>
2018-02-06 18:38:55 +0200

    auto login

---
+++ include/function/f_igosja_login_code.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Формуюємо код для автовходу через cookie
+ * @param $user_code string user_code з БД
+ * @return string
+ */
+function f_igosja_login_code($user_code)
+{
+    if (isset($_SERVER['REMOTE_ADDR']))
+    {
+        $user_ip = $_SERVER['REMOTE_ADDR'];
+    }
+    else
+    {
+        $user_ip = '';
+    }
+
+    if (isset($_SERVER['HTTP_USER_AGENT']))
+    {
+        $user_agent = $_SERVER['HTTP_USER_AGENT'];
+    }
+    else
+    {
+        $user_agent = '';
+    }
+
+    $login_code = md5($user_ip . $user_agent . $user_code);
+
+    return $login_code;
+}
--- login.php
+++ login.php
@@ -24,7 +24,8 @@ if ($data = f_igosja_request_post('data'))
     $login      = trim($data['login']);
     $password   = f_igosja_hash_password($data['password']);
 
-    $sql = "SELECT `user_id`
+    $sql = "SELECT `user_code`,
+                   `user_id`
             FROM `user`
             WHERE `user_login`=?
             AND `user_password`=?
@@ -47,6 +48,8 @@ if ($data = f_igosja_request_post('data'))
 
     $user_array             = $user_sql->fetch_all(MYSQLI_ASSOC);
     $_SESSION['user_id']    = $user_array[0]['user_id'];
+
+    setcookie('login_code', $user_array[0]['user_code'] . '-' . f_igosja_login_code($user_array[0]['user_code']), time() + 31536000); //365 днів
 
     redirect('/team_view.php');
 }
