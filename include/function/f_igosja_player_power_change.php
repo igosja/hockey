ed80242ce7916197b53f5d34f345435f3ce26c7b
Igor Boyko <igosja@ukr.net>
2018-02-09 22:45:14 +0200

    player change power icons

--- include/function/f_igosja_player_power_change.php
+++ include/function/f_igosja_player_power_change.php
@@ -11,9 +11,9 @@ function f_igosja_player_power_change($power_change)
     {
         $result = '<img alt="Получил балл силы по результатам матча" src="/img/plus.png" title="Получил балл силы по результатам матча" />';
     }
-    else
+    elseif ($power_change < 0)
     {
-        $result = '<img alt="Потерял балл силы по результатам матча" src="/img/plus.png" title="Потерял балл силы по результатам матча" />';
+        $result = '<img alt="Потерял балл силы по результатам матча" src="/img/minus.png" title="Потерял балл силы по результатам матча" />';
     }
 
     return $result;
