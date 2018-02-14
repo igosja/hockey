377df655fea431bc5125be56cfd6fa23c00deb42
Igor Boyko <igor.boyko@hotelfriend.co>
2018-02-14 13:36:48 +0200

    admin auto login

--- admin_login.php
+++ admin_login.php
@@ -25,7 +25,8 @@ if ($data = f_igosja_request_post('data'))
     $login      = trim($data['login']);
     $password   = f_igosja_hash_password($data['password']);
 
-    $sql = "SELECT `user_id`
+    $sql = "SELECT `user_code`,
+                   `user_id`
             FROM `user`
             WHERE `user_login`=?
             AND `user_password`=?
@@ -50,6 +51,8 @@ if ($data = f_igosja_request_post('data'))
     $user_array = $user_sql->fetch_all(MYSQLI_ASSOC);
 
     $_SESSION['admin_user_id'] = $user_array[0]['user_id'];
+
+    setcookie('admin_login_code', $user_array[0]['user_code'] . '-' . f_igosja_login_code($user_array[0]['user_code']), time() + 31536000); //365 днів
 
     redirect('/admin/');
 }
