6e8b841d3f15d44d51d34acaff70a311ee08d45f
Igor Boyko <igor.boyko@hotelfriend.co>
2018-02-20 13:34:49 +0200

    player price sql fix

--- console/folder/generator/function/f_igosja_generator_player_price_and_salary.php
+++ console/folder/generator/function/f_igosja_generator_player_price_and_salary.php
@@ -28,7 +28,7 @@ function f_igosja_generator_player_price_and_salary()
                 GROUP BY `playerposition_player_id`
             ) AS `t2`
             ON `playerposition_player_id`=`player_id`
-            SET `player_price`=POW(150-(28-`player_power_nominal`/2), 2)*`player_power_nominal`*(1+IF(`special_level` IS NULL, 0, `special_level`)/8)*(2+`position`)/3
+            SET `player_price`=POW(150-(28-`player_age`), 2)*(`position`-1+`player_power_nominal`+IFNULL(`special_level`, 0))
             WHERE `player_age`<40";
     f_igosja_mysqli_query($sql);
 
