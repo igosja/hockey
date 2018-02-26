7838ebe0d92bb9ff9770d0346277681d22b638bb
Igor Boyko <igor.boyko@hotelfriend.co>
2018-02-26 18:27:58 +0200

    admin vote update

--- admin/vote_update.php
+++ admin/vote_update.php
@@ -66,8 +66,8 @@ if ($data = f_igosja_request_post('data'))
             {
                 $sql = "INSERT INTO `voteanswer`
                         SET `voteanswer_text`='$item',
-                            `voteanswer_vote_id`=$num_get
-                        WHERE `voteanswer_id`=$key";
+                            `voteanswer_vote_id`=$num_get,
+                            `voteanswer_id`=$key";
                 f_igosja_mysqli_query($sql);
             }
         }
