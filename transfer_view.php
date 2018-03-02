b5fbfb8f6584ddd969286740488125410f37b1ff
Igor Boyko <igor.boyko@hotelfriend.co>
2018-03-02 18:11:00 +0200

    transfer rating

--- transfer_view.php
+++ transfer_view.php
@@ -77,6 +77,178 @@ if (0 == $transfer_sql->num_rows)
 
 $transfer_array = $transfer_sql->fetch_all(MYSQLI_ASSOC);
 
+if ($data = f_igosja_request_post('data'))
+{
+    if (isset($auth_user_id))
+    {
+        if (!isset($data['rating']))
+        {
+            f_igosja_session_front_flash_set('error', 'Укажите свою оценку сделки.');
+        }
+
+        $rating = (int) $data['rating'];
+
+        $sql = "SELECT COUNT(`country_id`) AS `count`
+                FROM `country`
+                WHERE `country_president_id`=$auth_user_id
+                OR `country_vice_id`=$auth_user_id";
+        $check_sql = f_igosja_mysqli_query($sql);
+
+        $check_array = $check_sql->fetch_all(MYSQLI_ASSOC);
+
+        if (0 == $check_array[0]['count'])
+        {
+            if ($rating > 0)
+            {
+                $rating = 1;
+            }
+            else
+            {
+                $rating = -1;
+            }
+        }
+        else
+        {
+            if ($rating > 0)
+            {
+                $rating = 10;
+            }
+            else
+            {
+                $rating = -10;
+            }
+        }
+
+        $sql = "SELECT COUNT(`transfer_id`) AS `check`
+                FROM `transfer`
+                WHERE `transfer_ready`=1
+                AND `transfer_id`=$num_get
+                AND `transfer_checked`=0
+                LIMIT 1";
+        $transfer_sql = f_igosja_mysqli_query($sql);
+
+        $transfer_array = $transfer_sql->fetch_all(MYSQLI_ASSOC);
+
+        if (0 == $transfer_array[0]['check'])
+        {
+            redirect('/wrong_page.php');
+        }
+
+        $sql = "SELECT COUNT(`transfervote_transfer_id`) AS `check`
+                FROM `transfervote`
+                WHERE `transfervote_transfer_id`=$num_get
+                AND `transfervote_user_id`=$auth_user_id";
+        $transfervote_sql = f_igosja_mysqli_query($sql);
+
+        $transfervote_array = $transfervote_sql->fetch_all(MYSQLI_ASSOC);
+
+        if (0 != $transfervote_array[0]['check'])
+        {
+            $sql = "UPDATE `transfervote`
+                    SET `transfervote_rating`=-$rating,
+                    WHERE `transfervote_transfer_id`=$num_get
+                    AND `transfervote_user_id`=$auth_user_id";
+            f_igosja_mysqli_query($sql);
+        }
+        else
+        {
+            $sql = "INSERT INTO `transfervote`
+                    SET `transfervote_rating`=-$rating,
+                        `transfervote_transfer_id`=$num_get,
+                        `transfervote_user_id`=$auth_user_id";
+            f_igosja_mysqli_query($sql);
+        }
+
+        f_igosja_session_front_flash_set('success', 'Ваш голос успешно сохранён.');
+
+        if (isset($data['text']))
+        {
+            $text = htmlspecialchars($data['text']);
+            $text = trim($text);
+
+            if (!empty($text))
+            {
+                $sql = "INSERT INTO `transfercomment`
+                        SET `transfercomment_date`=UNIX_TIMESTAMP(),
+                            `transfercomment_transfer_id`=$num_get,
+                            `transfercomment_text`=?,
+                            `transfercomment_user_id`=$auth_user_id";
+                $prepare = $mysqli->prepare($sql);
+                $prepare->bind_param('s', $text);
+                $prepare->execute();
+                $prepare->close();
+            }
+        }
+
+        if (in_array($rating, array(1, -1)))
+        {
+            refresh();
+        }
+
+        $sql = "SELECT `transfer_id`
+                FROM `transfer`
+                LEFT JOIN
+                (
+                    SELECT `transfervote_transfer_id`
+                    FROM `transfervote`
+                    WHERE `transfervote_transfer_id` IN
+                    (
+                        SELECT `transfer_id`
+                        FROM `transfer`
+                        WHERE `transfer_ready`=1
+                        AND `transfer_checked`=0
+                    )
+                    AND `transfervote_user_id`=$auth_user_id
+                ) AS `t1`
+                ON `transfer_id`=`transfervote_transfer_id`
+                WHERE `transfer_ready`=1
+                AND `transfer_checked`=0
+                AND `transfervote_transfer_id` IS NULL
+                ORDER BY `transfer_id` ASC
+                LIMIT 1";
+        $transfer_sql = f_igosja_mysqli_query($sql);
+
+        if (0 != $transfer_sql->num_rows)
+        {
+            $transfer_array = $transfer_sql->fetch_all(MYSQLI_ASSOC);
+
+            redirect('/transfer_view.php?num=' . $transfer_array[0]['transfer_id']);
+        }
+
+        $sql = "SELECT `rent_id`
+                FROM `rent`
+                LEFT JOIN
+                (
+                    SELECT `rentvote_rent_id`
+                    FROM `rentvote`
+                    WHERE `rentvote_rent_id` IN
+                    (
+                        SELECT `rent_id`
+                        FROM `rent`
+                        WHERE `rent_ready`=1
+                        AND `rent_checked`=0
+                    )
+                    AND `rentvote_user_id`=$auth_user_id
+                ) AS `t1`
+                ON `rent_id`=`rentvote_rent_id`
+                WHERE `rent_ready`=1
+                AND `rent_checked`=0
+                AND `rentvote_rent_id` IS NULL
+                ORDER BY `rent_id` ASC
+                LIMIT 1";
+        $rent_sql = f_igosja_mysqli_query($sql);
+
+        if (0 != $rent_sql->num_rows)
+        {
+            $rent_array = $rent_sql->fetch_all(MYSQLI_ASSOC);
+
+            redirect('/rent_view.php?num=' . $rent_array[0]['rent_id']);
+        }
+
+        redirect('/transfer_view.php?num=' . $num_get);
+    }
+}
+
 $sql = "SELECT `transferposition_transfer_id` AS `playerposition_player_id`,
                `position_name`,
                `position_short`
--- view/transfer_view.php
+++ view/transfer_view.php
@@ -24,15 +24,6 @@
         <?php include(__DIR__ . '/include/transfer_link.php'); ?>
     </div>
 </div>
-<?php if (0 == $rating_my) { ?>
-    <div class="row text-center margin">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <a href="/transfer_rating_plus.php?num=<?= $num_get; ?>">Честная сделка</a>
-            |
-            <a class="reject" href="javascript:">Нечестная сделка</a>
-        </div>
-    </div>
-<?php } ?>
 <div class="row">
     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
         <div class="row">
@@ -217,27 +208,35 @@
         </div>
     <?php } ?>
 <?php } ?>
-<?php if (0 == $rating_my) { ?>
-    <div class="deal-comment-block">
-        <div class="row margin-top">
-            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center strong">
-                <label for="transfercomment">Ваш комментарий:</label>
+<?php if (0 == $transfer_array[0]['transfer_checked']) { ?>
+    <div class="row margin-top">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center strong">
+            <label for="transfercomment">Ваше мнение:</label>
+        </div>
+    </div>
+    <form id="transfercomment-form" method="POST">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <label for="transferrating-plus"><input id="transferrating-plus" name="data[rating]" type="radio" value="1" /> Честная сделка</label>
+                <br />
+                <label for="transferrating-minus"><input id="transferrating-minus" name="data[rating]" type="radio" value="-1" /> Нечестная сделка</label>
             </div>
         </div>
-        <form action="/transfer_rating_minus.php?num=<?= $num_get; ?>" id="transfercomment-form" method="POST">
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-                    <textarea class="form-control" id="transfercomment" name="data[text]" rows="5"></textarea>
-                </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center transferrating-error notification-error"></div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <textarea class="form-control" id="transfercomment" name="data[text]" rows="5"></textarea>
             </div>
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center transfercomment-error notification-error"></div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center transfercomment-error notification-error"></div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+                <button class="btn margin">Сохранить</button>
             </div>
-            <div class="row">
-                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
-                    <button class="btn margin">Сохранить</button>
-                </div>
-            </div>
-        </form>
-    </div>
+        </div>
+    </form>
 <?php } ?>
