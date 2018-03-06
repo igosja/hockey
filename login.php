1805d55180d5572afe2bc2ad9a4afe8d716c98ca
Igor Boyko <igosja@ukr.net>
2018-03-06 21:59:26 +0200

    one computer

--- login.php
+++ login.php
@@ -47,6 +47,59 @@ if ($data = f_igosja_request_post('data'))
     $user_array             = $user_sql->fetch_all(MYSQLI_ASSOC);
     $_SESSION['user_id']    = $user_array[0]['user_id'];
 
+    if ($user_code = f_igosja_cookie('computer_code'))
+    {
+        $session_user_id = $_SESSION['user_id'];
+
+        $sql = "SELECT `user_id`
+                FROM `user`
+                WHERE `user_code`=?
+                AND `user_id`!=$session_user_id
+                LIMIT 1";
+        $prepare = $mysqli->prepare($sql);
+        $prepare->bind_param('s', $user_code);
+        $prepare->execute();
+
+        $user_sql = $prepare->get_result();
+
+        $prepare->close();
+
+        if (0 != $user_sql->num_rows)
+        {
+            $user_array = $user_sql->fetch_all(MYSQLI_ASSOC);
+
+            $user_id = $user_array[0]['user_id'];
+
+            $sql = "SELECT COUNT(`onecomputer_id`) AS `count`
+                    WHERE (`onecomputer_child_id`=$user_id
+                    AND `onecomputer_user_id`=$session_user_id)
+                    OR (`onecomputer_child_id`=$session_user_id
+                    AND `onecomputer_user_id`=$user_id)";
+            $check_sql = f_igosja_mysqli_query($sql);
+
+            $check_array = $check_sql->fetch_all(MYSQLI_ASSOC);
+
+            if (0 == $check_array[0]['count'])
+            {
+                $sql = "INSERT INTO `onecomputer`
+                        SET `onecomputer_date`=UNIX_TIMESTAMP(),
+                            `onecomputer_child_id`=$user_id,
+                            `onecomputer_user_id`=$session_user_id";
+                f_igosja_mysqli_query($sql);
+            }
+            else
+            {
+                $sql = "UPDATE `onecomputer`
+                        SET `onecomputer_date`=UNIX_TIMESTAMP(),
+                        WHERE (`onecomputer_child_id`=$user_id
+                        AND `onecomputer_user_id`=$session_user_id)
+                        OR (`onecomputer_child_id`=$session_user_id
+                        AND `onecomputer_user_id`=$user_id)";
+                f_igosja_mysqli_query($sql);
+            }
+        }
+    }
+
     setcookie('login_code', $user_array[0]['user_code'] . '-' . f_igosja_login_code($user_array[0]['user_code']), time() + 31536000); //365 днів
     setcookie('computer_code', $user_array[0]['user_code'], time() + 31536000); //365 днів
 
