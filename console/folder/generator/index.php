fcd47b3c63464e27b41846332ea898587b129aeb
Igor Boyko <igosja@ukr.net>
2018-03-09 12:02:47 +0200

    generator fix

--- console/folder/generator/index.php
+++ console/folder/generator/index.php
@@ -3,53 +3,53 @@
 include(__DIR__ . '/../../../include/generator.php');
 
 $function_array = array(
-    // 'f_igosja_generator_site_close',
-    // 'f_igosja_generator_player_power_new_to_old',
-    // 'f_igosja_generator_check_mood_limit',
-    // 'f_igosja_generator_check_lineup',
-    // 'f_igosja_generator_fill_lineup',
-    // 'f_igosja_generator_set_auto',
-    // 'f_igosja_generator_set_default_style',
-    // 'f_igosja_generator_set_user_auto',
-    // 'f_igosja_generator_set_ticket_price',
-    // 'f_igosja_generator_set_stadium',
-    // 'f_igosja_generator_count_visitor',
-    // 'f_igosja_generator_finance_stadium',
-    // 'f_igosja_generator_team_to_statistic',
-    // 'f_igosja_generator_user_to_rating',
-    // 'f_igosja_generator_lineup_to_statistic',
-    // 'f_igosja_generator_national_vs',
-    // 'f_igosja_generator_game_result',
-    // 'f_igosja_generator_league_coefficient',
-    // 'f_igosja_generator_update_team_statistic',
-    // 'f_igosja_generator_update_player_statistic',
-    // 'f_igosja_generator_user_rating',
-    // 'f_igosja_generator_country_auto',
-    // 'f_igosja_generator_team_visitor_after_game',
-    // 'f_igosja_generator_team_visitor',
-    // 'f_igosja_generator_plus_minus',
-    // 'f_igosja_generator_decrease_teamwork',
-    // 'f_igosja_generator_standing',
-    // 'f_igosja_generator_standing_place',
-    // 'f_igosja_generator_player_game_row',
-    // 'f_igosja_generator_player_tire',
-    // 'f_igosja_generator_training',
-    // 'f_igosja_generator_phisical',
-    // 'f_igosja_generator_school',
-    // 'f_igosja_generator_scout',
-    // 'f_igosja_generator_building_base',
-    // 'f_igosja_generator_building_stadium',
-    // 'f_igosja_generator_stadium_maintenance',
-    // 'f_igosja_generator_decrease_injury',
-    // 'f_igosja_generator_set_injury',
-    // 'f_igosja_generator_make_played',
-    // 'f_igosja_generator_league_out',
-    // 'f_igosja_generator_league_lot',
-    // 'f_igosja_generator_participant_championship',
-    // 'f_igosja_generator_playoff_championship_add_game',
-    // 'f_igosja_generator_playoff_championship_lot',
-    // 'f_igosja_generator_achievement',
-    // 'f_igosja_generator_prize',
+    'f_igosja_generator_site_close',
+    'f_igosja_generator_player_power_new_to_old',
+    'f_igosja_generator_check_mood_limit',
+    'f_igosja_generator_check_lineup',
+    'f_igosja_generator_fill_lineup',
+    'f_igosja_generator_set_auto',
+    'f_igosja_generator_set_default_style',
+    'f_igosja_generator_set_user_auto',
+    'f_igosja_generator_set_ticket_price',
+    'f_igosja_generator_set_stadium',
+    'f_igosja_generator_count_visitor',
+    'f_igosja_generator_finance_stadium',
+    'f_igosja_generator_team_to_statistic',
+    'f_igosja_generator_user_to_rating',
+    'f_igosja_generator_lineup_to_statistic',
+    'f_igosja_generator_national_vs',
+    'f_igosja_generator_game_result',
+    'f_igosja_generator_league_coefficient',
+    'f_igosja_generator_update_team_statistic',
+    'f_igosja_generator_update_player_statistic',
+    'f_igosja_generator_user_rating',
+    'f_igosja_generator_country_auto',
+    'f_igosja_generator_team_visitor_after_game',
+    'f_igosja_generator_team_visitor',
+    'f_igosja_generator_plus_minus',
+    'f_igosja_generator_decrease_teamwork',
+    'f_igosja_generator_standing',
+    'f_igosja_generator_standing_place',
+    'f_igosja_generator_player_game_row',
+    'f_igosja_generator_player_tire',
+    'f_igosja_generator_training',
+    'f_igosja_generator_phisical',
+    'f_igosja_generator_school',
+    'f_igosja_generator_scout',
+    'f_igosja_generator_building_base',
+    'f_igosja_generator_building_stadium',
+    'f_igosja_generator_stadium_maintenance',
+    'f_igosja_generator_decrease_injury',
+    'f_igosja_generator_set_injury',
+    'f_igosja_generator_make_played',
+    'f_igosja_generator_league_out',
+    'f_igosja_generator_league_lot',
+    'f_igosja_generator_participant_championship',
+    'f_igosja_generator_playoff_championship_add_game',
+    'f_igosja_generator_playoff_championship_lot',
+    'f_igosja_generator_achievement',
+    'f_igosja_generator_prize',
     'f_igosja_generator_swiss',
     'f_igosja_generator_rent_decrease_return',
     'f_igosja_generator_transfer',
@@ -90,23 +90,11 @@ $function_array = array(
     'f_igosja_generator_site_open',
 );
 
-$prev_query = f_igosja_get_count_query();
-$prev_time  = $start_time;
-
 for ($i=0, $count_function=count($function_array); $i<$count_function; $i++)
 {
     $function_array[$i]();
 
     f_igosja_console_progress($i+1, $count_function);
-
-    $current_query  = f_igosja_get_count_query();
-    $current_time   = microtime(true);
-
-    print round($current_time - $prev_time, 5) . ' sec.' . "\r\n"
-        . 'Database queries: ' . ($current_query - $prev_query) . "\r\n";
-
-    $prev_query = $current_query;
-    $prev_time  = $current_time;
 }
 
 print "\r\n"
