af0f8c01876e90dc665db2e10242e425fcb4726b
Igor Boyko <igor.boyko@hotelfriend.co>
2018-03-12 17:52:21 +0200

    bb decode

--- include/function/f_igosja_style_scout.php
+++ include/function/f_igosja_style_scout.php
@@ -9,7 +9,7 @@
 
 function f_igosja_style_scout($style_id, $count_scout)
 {
-    if (2 == $count_scout)
+    if ($count_scout >= 2)
     {
         $sql = "SELECT `style_id`,
                        `style_name`
