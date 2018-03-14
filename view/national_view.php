3979f8ca598d835aa319cae0e4597a5b618a3370
Igor Boyko <igor.boyko@hotelfriend.co>
2018-03-14 17:13:06 +0200

    national hide tire and phisical

--- view/national_view.php
+++ view/national_view.php
@@ -78,13 +78,23 @@
                     >
                         <?= $item['player_power_nominal']; ?>
                     </td>
-                    <td class="text-center"><?= $item['player_tire']; ?></td>
                     <td class="text-center">
-                        <img
-                            alt="<?= $item['phisical_name']; ?>"
-                            src="/img/phisical/<?= $item['phisical_id']; ?>.png"
-                            title="<?= $item['phisical_name']; ?>"
-                        />
+                        <?php if (isset($auth_national_id) && $auth_national_id == $num_get) { ?>
+                            <?= $item['player_tire']; ?>
+                        <?php } else { ?>
+                            ?
+                        <?php } ?>
+                    </td>
+                    <td class="text-center">
+                        <?php if (isset($auth_national_id) && $auth_national_id == $num_get) { ?>
+                            <img
+                                alt="<?= $item['phisical_name']; ?>"
+                                src="/img/phisical/<?= $item['phisical_id']; ?>.png"
+                                title="<?= $item['phisical_name']; ?>"
+                            />
+                        <?php } else { ?>
+                            ?
+                        <?php } ?>
                     </td>
                     <td class="text-center"><?= $item['player_power_real']; ?></td>
                     <td class="hidden-xs text-center"><?= f_igosja_player_special($item['player_id'], $playerspecial_array); ?></td>
