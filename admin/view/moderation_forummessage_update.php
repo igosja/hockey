63f00e28df43a14d3e70e63c7f3c5e08cf08ce9e
Igor Boyko <igor.boyko@hotelfriend.co>
2018-03-26 17:26:20 +0300

    moderation forummessage

---
+++ admin/view/moderation_forummessage_update.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @var $forummessage_array array
+ */
+?>
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+        <h3 class="page-header">Редактирование сообщения</h3>
+    </div>
+</div>
+<ul class="list-inline preview-links text-center">
+    <li>
+        <a class="btn btn-default" href="/admin/moderation_forummessage.php">
+            Список
+        </a>
+    </li>
+</ul>
+<form class="form-horizontal" method="POST">
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 table-responsive">
+            <table class="table table-striped table-bordered table-hover table-condensed">
+                <tr>
+                    <td class="text-right col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                        <label class="control-label" for="forummessage_name">Текст</label>
+                    </td>
+                    <td>
+                        <input
+                            class="form-control"
+                            id="forummessage_name"
+                            name="data[forummessage_name]"
+                            value="<?= $forummessage_array[0]['forummessage_name']; ?>"
+                        >
+                    </td>
+                </tr>
+            </table>
+        </div>
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+            <button class="btn btn-default">Сохранить</button>
+        </div>
+    </div>
+</form>
