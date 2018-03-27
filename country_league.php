c18f1dea0e87add9ecb789b1bde158922c5cae39
Igor Boyko <igor.boyko@hotelfriend.co>
2018-03-27 17:01:42 +0300

    country league

--- country_league.php
+++ country_league.php
@@ -20,10 +20,57 @@ $sql = "SELECT `leaguedistribution_group`,
                `leaguedistribution_season_id`
         FROM `leaguedistribution`
         WHERE `leaguedistribution_country_id`=$num_get
-        ORDER BY `leaguedistribution_season_id` DESC";
+        ORDER BY `leaguedistribution_season_id` DESC
+        LIMIT 1";
 $leaguedistribution_sql = f_igosja_mysqli_query($sql);
 
 $leaguedistribution_array = $leaguedistribution_sql->fetch_all(MYSQLI_ASSOC);
+
+$sql = "SELECT `participantleague_season_id`
+        FROM `participantleague`
+        LEFT JOIN `team`
+        ON `participantleague_team_id`=`team_id`
+        LEFT JOIN `stadium`
+        ON `team_stadium_id`=`stadium_id`
+        LEFT JOIN `city`
+        ON `stadium_city_id`=`city_id`
+        WHERE `city_country_id`=$num_get
+        ORDER BY `participantleague_season_id` DESC";
+$season_sql = f_igosja_mysqli_query($sql);
+
+$season_array = $season_sql->fetch_all(MYSQLI_ASSOC);
+
+for ($i=0; $i<$season_sql->num_rows; $i++)
+{
+    $season_id = $season_array[$i]['participantleague_season_id'];
+
+    $sql = "SELECT `city_name`,
+                   `team_id`,
+                   `team_name`,
+                   `leaguecoefficient_loose`,
+                   `leaguecoefficient_loose_over`+`leaguecoefficient_loose_bullet` AS `leaguecoefficient_loose_over`,
+                   `leaguecoefficient_point`,
+                   `leaguecoefficient_win`,
+                   `leaguecoefficient_win_over`+`leaguecoefficient_win_bullet` AS `leaguecoefficient_win_over`,
+            FROM `participantleague`
+            LEFT JOIN `team`
+            ON `participantleague_team_id`=`team_id`
+            LEFT JOIN `stadium`
+            ON `team_stadium_id`=`stadium_id`
+            LEFT JOIN `city`
+            ON `stadium_city_id`=`city_id`
+            LEFT JOIN `leaguecoefficient`
+            ON (`team_id`=`leaguecoefficient_team_id`
+            AND `participantleague_season_id`=`leaguecoefficient_team_id`)
+            LEFT JOIN `stage`
+            ON `participantleague_stage_in`=`stage_id`
+            WHERE `city_country_id`=$num_get
+            AND `participantleague_season_id`=$season_id
+            ORDER BY `leaguecoefficient_point` DESC";
+    $team_sql = f_igosja_mysqli_query($sql);
+
+    $season_array[$i]['team'] = $team_sql->fetch_all(MYSQLI_ASSOC);
+}
 
 $seo_title          = $country_array[0]['country_name'] . '. Лига чемпионов';
 $seo_description    = $country_array[0]['country_name'] . '. Лига чемпионов на сайте Вирутальной Хоккейной Лиги.';
--- view/country_league.php
+++ view/country_league.php
@@ -1,6 +1,7 @@
 <?php
 /**
  * @var $leaguedistribution_array array
+ * @var $season_array array
  */
 ?>
 <?php include(__DIR__ . '/include/country_view.php'); ?>
@@ -33,3 +34,47 @@
         </table>
     </div>
 </div>
+<?php foreach ($season_array as $season) { ?>
+    <div class="row margin-top-small">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center strong">
+            <?= $season['participantleague_season_id']; ?> сезон
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 table-responsive">
+            <table class="table table-bordered table-hover">
+                <tr>
+                    <th>Команда</th>
+                    <th class="col-5" title="Победы">В</th>
+                    <th class="col-5" title="Победы в овертайте/по буллитам">ВО</th>
+                    <th class="col-5" title="Поражения в овертайте/по буллитам">ПО</th>
+                    <th class="col-5" title="Поражения">П</th>
+                    <th class="col-5" title="Очки">О</th>
+                </tr>
+                <?php foreach ($season['team'] as $item) { ?>
+                    <tr>
+                        <td>
+                            <a href="/team_view.php?num=<?= $item['team_id']; ?>">
+                                <?= $item['team_name']; ?>
+                                <span class="hidden-xs">(<?= $item['city_name']; ?>)</span>
+                            </a>
+                        </td>
+                        <td class="text-center"><?= $item['leaguecoefficient_win']; ?></td>
+                        <td class="text-center"><?= $item['leaguecoefficient_win_over']; ?></td>
+                        <td class="text-center"><?= $item['leaguecoefficient_loose_over']; ?></td>
+                        <td class="text-center"><?= $item['leaguecoefficient_loose']; ?></td>
+                        <td class="text-center strong"><?= $item['leaguecoefficient_point']; ?></td>
+                    </tr>
+                <?php } ?>
+                <tr>
+                    <th>Команда</th>
+                    <th title="Победы">В</th>
+                    <th title="Победы в овертайте/по буллитам">ВО</th>
+                    <th title="Поражения в овертайте/по буллитам">ПО</th>
+                    <th title="Поражения">П</th>
+                    <th title="Очки">О</th>
+                </tr>
+            </table>
+        </div>
+    </div>
+<?php } ?>
