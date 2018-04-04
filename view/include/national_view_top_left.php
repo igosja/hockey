032c344c8193f96b794d3baefc4c3fb6bb838753
Igor Boyko <igor.boyko@hotelfriend.co>
2018-04-04 15:55:22 +0300

    national relation

--- view/include/national_view_top_left.php
+++ view/include/national_view_top_left.php
@@ -86,7 +86,7 @@
     </div>
 </div>
 <div class="row">
-    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-right strong">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 margin-top-small">
         Рейтинг тренера:
     </div>
     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
