07da80e48adc965de965b6e308c9754949ddefc4
Igor Boyko <igor.boyko@hotelfriend.co>
2018-04-05 18:29:09 +0300

    pagination

--- view/include/pagination.php
+++ view/include/pagination.php
@@ -3,10 +3,10 @@
 ?>
 <div class="row margin-top-small">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
-        <a class="btn btn-default" href="javascript:">1</a>
-        <a class="btn" href="javascript:">25</a>
+        <a class="btn pagination" href="javascript:">1</a>
+        <a class="btn pagination" href="javascript:">25</a>
         <span class="btn">26</span>
-        <a class="btn btn-default" href="javascript:">27</a>
-        <a class="btn btn-default" href="javascript:">99</a>
+        <a class="btn pagination" href="javascript:">27</a>
+        <a class="btn pagination" href="javascript:">99</a>
     </div>
 </div>
