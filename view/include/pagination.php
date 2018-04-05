b38df9491320465fb08d2374b84dd378089ad4ec
Igor Boyko <igor.boyko@hotelfriend.co>
2018-04-05 18:15:17 +0300

    pagination

--- view/include/pagination.php
+++ view/include/pagination.php
@@ -1,12 +1,14 @@
 <?php
 
 ?>
-<div class="row pagination">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <a class="btn" href="javascript:">1</a>
-        <a class="btn" href="javascript:">5</a>
-        <span class="btn">6</span>
-        <a class="btn" href="javascript:">7</a>
-        <a class="btn" href="javascript:">10</a>
+<div class="row margin-top-small">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+        <ul class="pagination">
+            <li><a href="javascript:">1</a></li>
+            <li><a href="javascript:">5</a></li>
+            <li><span>6</span></li>
+            <li><a href="javascript:">7</a></li>
+            <li><a href="javascript:">10</a></li>
+        </ul>
     </div>
 </div>
