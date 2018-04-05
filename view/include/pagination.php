e3a1845572eb2fa44fe8759f83617d91f95ef497
Igor Boyko <igor.boyko@hotelfriend.co>
2018-04-05 18:08:47 +0300

    pagination

--- view/include/pagination.php
+++ view/include/pagination.php
@@ -3,10 +3,10 @@
 ?>
 <div class="row pagination">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <a href="javascript:">1</a>
-        <a href="javascript:">5</a>
-        <span>6</span>
-        <a href="javascript:">7</a>
-        <a href="javascript:">10</a>
+        <a class="btn" href="javascript:">1</a>
+        <a class="btn" href="javascript:">5</a>
+        <span class="btn">6</span>
+        <a class="btn" href="javascript:">7</a>
+        <a class="btn" href="javascript:">10</a>
     </div>
 </div>
