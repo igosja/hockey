8ea1945872305cf52f994e80e2843bf9810388d9
Igor Boyko <igor.boyko@hotelfriend.co>
2018-04-18 12:27:59 +0300

    generator bug

--- console/folder/generator/secondary/f_igosja_swiss_game.php
+++ console/folder/generator/secondary/f_igosja_swiss_game.php
@@ -7,7 +7,7 @@
  */
 function f_igosja_swiss_game($tournamenttype_id)
 {
-    $position_difference = 100;
+    $position_difference = 1;
 
     $team_array = f_igosja_swiss_prepare($tournamenttype_id);
     $game_array = f_igosja_swiss($tournamenttype_id, $position_difference, $team_array);
