142ec71462d13874e5e7799119de99e164f1a49f
Igor Boyko <igor.boyko@hotelfriend.co>
2018-04-18 12:14:52 +0300

    generator bug

--- include/function/f_igosja_console_progress.php
+++ include/function/f_igosja_console_progress.php
@@ -4,11 +4,12 @@
  * Друкуємо рядок в консолі з інформацією про відсоток виконаних функцій під час генерації ігрового дня
  * @param $current integer номер поточної функції
  * @param $total integer загальна кількість функцій
+ * @param $name string назва функції
  */
-function f_igosja_console_progress($current, $total)
+function f_igosja_console_progress($current, $total, $name)
 {
     $percent = round($current / $total * 100, 1);
 
-    print "\r" . $percent . '%     ';
+    print "\r" . $percent . '% - ' . $name . '                                         ';
     flush();
 }
