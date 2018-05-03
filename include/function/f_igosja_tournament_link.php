adb16c233c1cf4891fd1ff84e2575dc8547c1148
Igor Boyko <igor.boyko@hotelfriend.co>
2018-05-03 17:42:51 +0300

    tournamtnt link on game page

--- include/function/f_igosja_tournament_link.php
+++ include/function/f_igosja_tournament_link.php
@@ -8,11 +8,11 @@
 
 function f_igosja_game_tournament_link($game_array)
 {
-    if (TOURNAMENTTYPE_NATIONAL)
+    if (TOURNAMENTTYPE_NATIONAL == $game_array[0]['stage_id'])
     {
         $result = '<a href="/worldcup.php?season_id=' . $game_array[0]['schedule_season_id'] . '">' . $game_array[0]['tournamenttype_name'] . ', ' . $game_array[0]['stage_name'] . '</a>';
     }
-    elseif (TOURNAMENTTYPE_LEAGUE)
+    elseif (TOURNAMENTTYPE_LEAGUE == $game_array[0]['stage_id'])
     {
         if ($game_array[0]['stage_id'] <= STAGE_6_TOUR)
         {
@@ -29,15 +29,15 @@ function f_igosja_game_tournament_link($game_array)
 
         $result = '<a href="/league.php?season_id=' . $game_array[0]['schedule_season_id'] . '&stage_id=' . $game_array[0]['stage_id'] . '&round_id=' . $round_id . '">' . $game_array[0]['tournamenttype_name'] . ', ' . $game_array[0]['stage_name'] . '</a>';
     }
-    elseif (TOURNAMENTTYPE_CHAMPIONSHIP)
+    elseif (TOURNAMENTTYPE_CHAMPIONSHIP == $game_array[0]['stage_id'])
     {
         $result = '<a href="/championship.php?country_id=' . $game_array[0]['championship_country_id'] . '&division_id=' . $game_array[0]['championship_division_id'] . '&season_id=' . $game_array[0]['schedule_season_id'] . '">' . $game_array[0]['tournamenttype_name'] . ', ' . $game_array[0]['stage_name'] . '</a>';
     }
-    elseif (TOURNAMENTTYPE_CONFERENCE)
+    elseif (TOURNAMENTTYPE_CONFERENCE == $game_array[0]['stage_id'])
     {
         $result = '<a href="/conference_table.php?season_id=' . $game_array[0]['schedule_season_id'] . '">' . $game_array[0]['tournamenttype_name'] . ', ' . $game_array[0]['stage_name'] . '</a>';
     }
-    elseif (TOURNAMENTTYPE_OFFSEASON)
+    elseif (TOURNAMENTTYPE_OFFSEASON == $game_array[0]['stage_id'])
     {
         $result = '<a href="/offseason_table.php?season_id=' . $game_array[0]['schedule_season_id'] . '">' . $game_array[0]['tournamenttype_name'] . ', ' . $game_array[0]['stage_name'] . '</a>';
     }
