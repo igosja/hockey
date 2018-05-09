82b9d2d221a6fe57c85eda5986f05413d4da99d4
igosja <igosja@ukr.net>
2018-05-09 17:43:54 +0300

    generator

--- 123-fix-generator.php
+++ 123-fix-generator.php
@@ -100,12 +100,97 @@ for ($i=0; $i<2; $i++)
     }
 }
 
-$game_result = f_igosja_count_player_bonus($game_result);
-$game_result = f_igosja_get_teamwork($game_result);
-$game_result = f_igosja_set_teamwork($game_result);
-$game_result = f_igosja_collision($game_result);
-$game_result = f_igosja_player_optimal_power($game_result);
-$game_result = f_igosja_get_player_real_power_from_optimal($game_result);
+for ($i=0; $i<2; $i++)
+{
+    if (0 == $i)
+    {
+        $team = TEAM_HOME;
+    }
+    else
+    {
+        $team = TEAM_GUEST;
+    }
+
+    $game_result[$team]['player']['gk']['power_optimal'] = round(
+        $game_result[$team]['player']['gk']['power_optimal']
+        * (10 - $game_result[$team]['team']['mood'] + 2) / 10
+    );
+
+    for ($line=1; $line<=3; $line++)
+    {
+        for ($k=POSITION_LD; $k<=POSITION_RW; $k++)
+        {
+            if     (POSITION_LD == $k) { $key = 'ld'; }
+            elseif (POSITION_RD == $k) { $key = 'rd'; }
+            elseif (POSITION_LW == $k) { $key = 'lw'; }
+            elseif (POSITION_C  == $k) { $key =  'c'; }
+            else                       { $key = 'rw'; }
+
+            $key = $key . '_' . $line;
+
+            if (in_array($k, array(POSITION_LD, POSITION_RD)))
+            {
+                if (TACTIC_ATACK_SUPER == $game_result[$team]['team']['tactic'][$line])
+                {
+                    $tactic = -10 / 2;
+                }
+                elseif (TACTIC_ATACK == $game_result[$team]['team']['tactic'][$line])
+                {
+                    $tactic = -5 / 2;
+                }
+                elseif (TACTIC_DEFENCE == $game_result[$team]['team']['tactic'][$line])
+                {
+                    $tactic = 5 / 2;
+                }
+                elseif (TACTIC_DEFENCE_SUPER == $game_result[$team]['team']['tactic'][$line])
+                {
+                    $tactic = 10 / 2;
+                }
+                else
+                {
+                    $tactic = 0;
+                }
+            }
+            else
+            {
+                if (TACTIC_ATACK_SUPER == $game_result[$team]['team']['tactic'][$line])
+                {
+                    $tactic = 10 / 3;
+                }
+                elseif (TACTIC_ATACK == $game_result[$team]['team']['tactic'][$line])
+                {
+                    $tactic = 5 / 3;
+                }
+                elseif (TACTIC_DEFENCE == $game_result[$team]['team']['tactic'][$line])
+                {
+                    $tactic = -5 / 3;
+                }
+                elseif (TACTIC_DEFENCE_SUPER == $game_result[$team]['team']['tactic'][$line])
+                {
+                    $tactic = -10 / 3;
+                }
+                else
+                {
+                    $tactic = 0;
+                }
+            }
+
+            $game_result[$team]['player']['field'][$key]['power_optimal'] = round(
+                $game_result[$team]['player']['field'][$key]['power_optimal']
+                * (10 - $game_result[$team]['team']['mood'] + 2) / 10
+                * (100 + $game_result[$team]['team']['rude'][$line] - 1) / 100
+                * (100 + $tactic) / 100
+            );
+        }
+    }
+}
+
+//$game_result = f_igosja_count_player_bonus($game_result);
+//$game_result = f_igosja_get_teamwork($game_result);
+//$game_result = f_igosja_set_teamwork($game_result);
+//$game_result = f_igosja_collision($game_result);
+//$game_result = f_igosja_player_optimal_power($game_result);
+//$game_result = f_igosja_get_player_real_power_from_optimal($game_result);
 $game_result = f_igosja_team_power($game_result);
 $game_result = f_igosja_team_power_forecast($game_result);
 $game_result = f_igosja_optimality($game_result);
