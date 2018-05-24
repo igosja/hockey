3919b231a9ce6d98348692151627ae04cb99ef27
Igor Boyko <igor.boyko@hotelfriend.co>
2018-05-24 14:39:57 +0300

    admin access

--- include/admin_access.php
+++ include/admin_access.php
@@ -12,5 +12,11 @@ if ('admin' == $chapter)
         redirect('/admin_login.php');
     }
 
+    if (!in_array(f_igosja_get_user_ip(), array('185.38.209.242')))
+    {
+        unset($_SESSION['admin_user_id']);
+        redirect('/admin_login.php');
+    }
+
     include(__DIR__ . '/../include/filter.php');
 }
