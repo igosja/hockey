254ecd2bf87eb06f68982af3bf7c52c67652982b
igosja <igosja@ukr.net>
2018-06-03 13:12:04 +0300

    style sort

--- admin/view/moderation_rentcomment.php
+++ admin/view/moderation_rentcomment.php
@@ -5,7 +5,7 @@
 ?>
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
-        <h3 class="page-header">Комментарий к адренде</h3>
+        <h3 class="page-header">Комментарий к аренде</h3>
     </div>
 </div>
 <ul class="list-inline preview-links text-center">
