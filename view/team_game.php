0dfd7b062556615c2e91db3de70c07bc83fbcb8c
Igor Boyko <igor.boyko@hotelfriend.co>
2018-06-06 16:55:59 +0300

    game score color in team game list

--- view/team_game.php
+++ view/team_game.php
@@ -73,8 +73,8 @@
                         </a>
                     </td>
                     <td class="hidden-xs text-center"><?= f_igosja_game_auto($item['game_auto']); ?></td>
-                    <td class="text-center <?php if ($item['game_home_score'] > $item['game_guest_score']) { ?>success<?php } elseif ($item['game_home_score'] < $item['game_guest_score']) { ?>error<?php } ?>">
-                        <a href="/game_view.php?num=<?= $item['game_id']; ?>">
+                    <td class="text-center">
+                        <a class="<?php if ($item['game_home_score'] > $item['game_guest_score']) { ?>success<?php } elseif ($item['game_home_score'] < $item['game_guest_score']) { ?>error<?php } ?>" href="/game_view.php?num=<?= $item['game_id']; ?>">
                             <?= f_igosja_game_score($item['game_played'], $item['game_home_score'], $item['game_guest_score']); ?>
                         </a>
                     </td>
