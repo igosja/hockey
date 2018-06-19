bbd6ea68a4b4d1ccc3b612238dfe91e52abb4bee
Igor Boyko <igor.boyko@hotelfriend.co>
2018-06-19 12:35:29 +0300

    conference table

---
+++ console/migrations/m180619_093316_conference.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180619_093316_conference
+ */
+class m180619_093316_conference extends Migration
+{
+    const TABLE = '{{%conference}}';
+
+    /**
+     * @return bool|void
+     */
+    public function safeUp()
+    {
+        $this->createTable(self::TABLE, [
+            'conference_id' => $this->primaryKey(11),
+            'conference_difference' => $this->integer(3)->defaultValue(0),
+            'conference_game' => $this->integer(2)->defaultValue(0),
+            'conference_guest' => $this->integer(2)->defaultValue(0),
+            'conference_home' => $this->integer(2)->defaultValue(0),
+            'conference_loose' => $this->integer(2)->defaultValue(0),
+            'conference_loose_bullet' => $this->integer(2)->defaultValue(0),
+            'conference_loose_over' => $this->integer(2)->defaultValue(0),
+            'conference_pass' => $this->integer(3)->defaultValue(0),
+            'conference_place' => $this->integer(2)->defaultValue(0),
+            'conference_point' => $this->integer(2)->defaultValue(0),
+            'conference_score' => $this->integer(3)->defaultValue(0),
+            'conference_season_id' => $this->integer(5)->defaultValue(0),
+            'conference_team_id' => $this->integer(5)->defaultValue(0),
+            'conference_win' => $this->integer(2)->defaultValue(0),
+            'conference_win_bullet' => $this->integer(2)->defaultValue(0),
+            'conference_win_over' => $this->integer(2)->defaultValue(0),
+        ]);
+    }
+
+    /**
+     * @return bool|void
+     */
+    public function safeDown()
+    {
+        $this->dropTable(self::TABLE);
+    }
+}
