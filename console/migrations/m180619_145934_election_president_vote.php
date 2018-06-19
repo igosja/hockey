433b4775daf408b22ecb693d07fc930f69eb990d
Igor Boyko <igor.boyko@hotelfriend.co>
2018-06-19 18:01:43 +0300

    election president vote table

---
+++ console/migrations/m180619_145934_election_president_vote.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180619_145934_election_president_vote
+ */
+class m180619_145934_election_president_vote extends Migration
+{
+    const TABLE = '{{%election_president_vote}}';
+
+    /**
+     * @return bool|void
+     */
+    public function safeUp()
+    {
+        $this->createTable(self::TABLE, [
+            'election_president_vote_id' => $this->primaryKey(11),
+            'election_president_vote_date' => $this->integer(11)->defaultValue(0),
+            'election_president_vote_election_president_id' => $this->integer(11)->defaultValue(0),
+            'election_president_vote_election_president_application_id' => $this->integer(11)->defaultValue(0),
+            'election_president_vote_user_id' => $this->integer(11)->defaultValue(0),
+        ]);
+    }
+
+    /**
+     * @return bool|void
+     */
+    public function safeDown()
+    {
+        $this->dropTable(self::TABLE);
+    }
+}
