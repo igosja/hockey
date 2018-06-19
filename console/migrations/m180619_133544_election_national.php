bc88f6c15f82dd1a823ff8f2ffac63995afdc999
Igor Boyko <igor.boyko@hotelfriend.co>
2018-06-19 16:46:15 +0300

    election national application table

--- console/migrations/m180619_133544_election_national.php
+++ console/migrations/m180619_133544_election_national.php
@@ -15,7 +15,7 @@ class m180619_133544_election_national extends Migration
     public function safeUp()
     {
         $this->createTable(self::TABLE, [
-            'election_national_id' => $this->primaryKey(1),
+            'election_national_id' => $this->primaryKey(11),
             'election_national_country_id' => $this->integer(3)->defaultValue(0),
             'election_national_date' => $this->integer(11)->defaultValue(0),
             'election_national_election_status_id' => $this->integer(1)->defaultValue(0),
