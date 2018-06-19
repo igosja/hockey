b42c1596b23fd1441a2f7613a1fdd432f8e2bd73
Igor Boyko <igor.boyko@hotelfriend.co>
2018-06-19 11:11:13 +0300

    building base table

---
+++ console/migrations/m180619_080833_building_base.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180619_080833_building_base
+ */
+class m180619_080833_building_base extends Migration
+{
+    const TABLE = '{{%building_base}}';
+
+    /**
+     * @return bool|void
+     */
+    public function safeUp()
+    {
+        $this->createTable(self::TABLE, [
+            'building_base_id' => $this->primaryKey(11),
+            'building_base_building_id' => $this->integer(11)->defaultValue(0),
+            'building_base_construction_type_id' => $this->integer(11)->defaultValue(0),
+            'building_base_day' => $this->integer(2)->defaultValue(0),
+            'building_base_ready' => $this->integer(11)->defaultValue(0),
+            'building_base_team_id' => $this->integer(5)->defaultValue(0),
+        ]);
+
+        $this->batchInsert(self::TABLE, ['building_name'], [
+            ['base'],
+            ['base_medical'],
+            ['base_physical'],
+            ['base_school'],
+            ['base_scout'],
+            ['base_training'],
+        ]);
+    }
+
+    /**
+     * @return bool|void
+     */
+    public function safeDown()
+    {
+        $this->dropTable(self::TABLE);
+    }
+}
