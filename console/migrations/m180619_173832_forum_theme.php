4a04c79d4c5f89d039a86f7e151dc77cb7904e97
igosja <igosja@ukr.net>
2018-06-19 20:42:47 +0300

    forum theme table

---
+++ console/migrations/m180619_173832_forum_theme.php
@@ -0,0 +1,34 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180619_173832_forum_theme
+ */
+class m180619_173832_forum_theme extends Migration
+{
+    const TABLE = '{{%forum_theme}}';
+
+    /**
+     * @return bool|void
+     */
+    public function safeUp()
+    {
+        $this->createTable(self::TABLE, [
+            'forum_theme_id' => $this->primaryKey(11),
+            'forum_theme_count_view' => $this->integer(11)->defaultValue(0),
+            'forum_theme_date' => $this->integer(11)->defaultValue(0),
+            'forum_theme_forum_group_id' => $this->integer(11)->defaultValue(0),
+            'forum_theme_name' => $this->string(255),
+            'forum_theme_user_id' => $this->integer(11)->defaultValue(0),
+        ]);
+    }
+
+    /**
+     * @return bool|void
+     */
+    public function safeDown()
+    {
+        $this->dropTable(self::TABLE);
+    }
+}
