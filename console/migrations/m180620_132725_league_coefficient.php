1d4e3e523713bfe685911407d8d98fe54f94790d
Igor Boyko <igor.boyko@hotelfriend.co>
2018-06-20 16:32:25 +0300

    league coefficient table

---
+++ console/migrations/m180620_132725_league_coefficient.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180620_132725_league_coefficient
+ */
+class m180620_132725_league_coefficient extends Migration
+{
+    const TABLE = '{{%league_coefficient}}';
+
+    /**
+     * @return bool|void
+     */
+    public function safeUp()
+    {
+        $this->createTable(self::TABLE, [
+            'league_coefficient_id' => $this->primaryKey(11),
+            'league_coefficient_country_id' => $this->integer(3)->defaultValue(0),
+            'league_coefficient_loose' => $this->integer(2)->defaultValue(0),
+            'league_coefficient_loose_overtime' => $this->integer(2)->defaultValue(0),
+            'league_coefficient_loose_shootout' => $this->integer(2)->defaultValue(0),
+            'league_coefficient_point' => $this->integer(2)->defaultValue(0),
+            'league_coefficient_season_id' => $this->integer(5)->defaultValue(0),
+            'league_coefficient_team_id' => $this->integer(11)->defaultValue(0),
+            'league_coefficient_win' => $this->integer(2)->defaultValue(0),
+            'league_coefficient_win_overtime' => $this->integer(2)->defaultValue(0),
+            'league_coefficient_win_shootout' => $this->integer(2)->defaultValue(0),
+        ]);
+    }
+
+    /**
+     * @return bool|void
+     */
+    public function safeDown()
+    {
+        $this->dropTable(self::TABLE);
+    }
+}
