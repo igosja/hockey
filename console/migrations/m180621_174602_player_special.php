f76d4bbf1d8996864b8fe201b8fdbd8de07aca33
igosja <igosja@ukr.net>
2018-06-21 20:47:50 +0300

    player position table

---
+++ console/migrations/m180621_174602_player_special.php
@@ -0,0 +1,32 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180621_174602_player_special
+ */
+class m180621_174602_player_special extends Migration
+{
+    const TABLE = '{{%player_special}}';
+
+    /**
+     * @return bool|void
+     */
+    public function safeUp()
+    {
+        $this->createTable(self::TABLE, [
+            'player_special_id' => $this->primaryKey(11),
+            'player_special_level' => $this->integer(1)->defaultValue(0),
+            'player_special_player_id' => $this->integer(11)->defaultValue(0),
+            'player_special_special_id' => $this->integer(2)->defaultValue(0),
+        ]);
+    }
+
+    /**
+     * @return bool|void
+     */
+    public function safeDown()
+    {
+        $this->dropTable(self::TABLE);
+    }
+}
