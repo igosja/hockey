ef22d01355d454560c568c3784b6e964c9d765b7
Igor Boyko <igor.boyko@hotelfriend.co>
2018-06-21 17:18:24 +0300

    participant league table

--- console/migrations/m180621_135819_participant_championship.php
+++ console/migrations/m180621_135819_participant_championship.php
@@ -15,15 +15,14 @@ class m180621_135819_participant_championship extends Migration
     public function safeUp()
     {
         $this->createTable(self::TABLE, [
-
             'participant_championship_id' => $this->primaryKey(11),
             'participant_championship_country_id' => $this->integer(3)->defaultValue(0),
             'participant_championship_division_id' => $this->integer(1)->defaultValue(0),
             'participant_championship_season_id' => $this->integer(3)->defaultValue(0),
-            'participant_championship_stage_id' => $this->integer(2)->defaultValue(0),
             'participant_championship_stage_1' => $this->integer(1)->defaultValue(0),
             'participant_championship_stage_2' => $this->integer(1)->defaultValue(0),
             'participant_championship_stage_4' => $this->integer(1)->defaultValue(0),
+            'participant_championship_stage_id' => $this->integer(2)->defaultValue(0),
             'participant_championship_team_id' => $this->integer(11)->defaultValue(0),
         ]);
     }
