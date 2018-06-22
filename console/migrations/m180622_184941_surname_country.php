d0316c0a7b2064f21953ce22423630cb9a56db3f
igosja <igosja@ukr.net>
2018-06-22 21:51:17 +0300

    surname country table

---
+++ console/migrations/m180622_184941_surname_country.php
@@ -0,0 +1,31 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m180622_184941_surname_country
+ */
+class m180622_184941_surname_country extends Migration
+{
+    const TABLE = '{{%surname_country}}';
+
+    /**
+     * @return bool|void
+     */
+    public function safeUp()
+    {
+        $this->createTable(self::TABLE, [
+            'surname_country_id' => $this->primaryKey(11),
+            'surname_country_country_id' => $this->integer(3)->defaultValue(0),
+            'surname_country_surname_id' => $this->integer(11)->defaultValue(0),
+        ]);
+    }
+
+    /**
+     * @return bool|void
+     */
+    public function safeDown()
+    {
+        $this->dropTable(self::TABLE);
+    }
+}
