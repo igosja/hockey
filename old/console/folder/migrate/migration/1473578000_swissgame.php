852aef2554aea254c37d89e7b955f12bd8de5c2c
igosja <igosja@ukr.net>
2018-06-22 22:07:35 +0300

    swiss table

--- old/console/folder/migrate/migration/1473578000_swissgame.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$q = array();
-
-$q[] = 'CREATE TABLE `swissgame`
-        (
-            `swissgame_guest_team_id` INT(11) DEFAULT 0,
-            `swissgame_home_team_id` INT(11) DEFAULT 0
-        );';
