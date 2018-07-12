16233d4a48665d150ebd434c5686c89803162d17
Igor Boyko <igor.boyko@hotelfriend.co>
2018-07-12 16:02:09 +0300

    news

--- frontend/views/news/index.php
+++ frontend/views/news/index.php
@@ -8,20 +8,23 @@ use yii\widgets\ListView;
  */
 
 ?>
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <h1>News</h1>
-        </div>
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        <h1>News</h1>
     </div>
-<?php
+</div>
+<div class="row">
+    <?php
 
-try {
-    print ListView::widget([
-        'dataProvider' => $dataProvider,
-        'itemView' => '_item',
-    ]);
-} catch (Exception $e) {
-    ErrorHelper::log($e);
-}
+    try {
+        print ListView::widget([
+            'dataProvider' => $dataProvider,
+            'itemView' => '_item',
+            'options' => ['class' => 'col-lg-12 col-md-12 col-sm-12 col-xs-12'],
+        ]);
+    } catch (Exception $e) {
+        ErrorHelper::log($e);
+    }
 
-?>
+    ?>
+</div>
