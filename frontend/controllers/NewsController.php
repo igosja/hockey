97f5afb13ed37b5203b8c6df992cd47a991c8799
Igor Boyko <igor.boyko@hotelfriend.co>
2018-07-12 16:15:44 +0300

    news

--- frontend/controllers/NewsController.php
+++ frontend/controllers/NewsController.php
@@ -4,6 +4,7 @@ namespace frontend\controllers;
 
 use common\models\News;
 use yii\data\ActiveDataProvider;
+use yii\db\ActiveQuery;
 
 /**
  * Class NewsController
@@ -16,10 +17,23 @@ class NewsController extends BaseController
      */
     public function actionIndex(): string
     {
+        $query = News::find()
+            ->with([
+                'newsComment' => function (ActiveQuery $query): ActiveQuery {
+                    return $query->select(['news_comment_news_id']);
+                },
+                'user' => function (ActiveQuery $query): ActiveQuery {
+                    return $query->select(['user_id', 'user_login']);
+                },
+            ])
+            ->select(['news_id', 'news_date', 'news_text', 'news_title', 'news_user_id'])
+            ->where(['news_country_id' => 0])
+            ->orderBy(['news_id' => SORT_DESC]);
+
         $dataProvider = new ActiveDataProvider([
-            'query' => News::find()->where(['news_country_id' => 0])->orderBy(['news_id' => SORT_DESC]),
+            'query' => $query,
             'pagination' => [
-                'pageSize' => News::PAGE_LIMIT,
+                'pageSize' => 1,//News::PAGE_LIMIT,
             ],
         ]);
 
