97c8e48158d3f2f9077704e616cff374752a2e46
igosja <igosja@ukr.net>
2018-09-28 21:20:28 +0300

    debug

--- environments/dev/console/config/main-local.php
+++ environments/dev/console/config/main-local.php
@@ -1,13 +1,9 @@
 <?php
 
 return [
-    'bootstrap' => ['gii'],
     'components' => [
         'urlManager' => [
             'hostInfo' => 'http://igosja.bget.ru/',
         ]
-    ],
-    'modules' => [
-        'gii' => 'yii\gii\Module',
     ],
 ];
