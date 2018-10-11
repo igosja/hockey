58471f20dfc9d11d556549a55c20650d3fe3666d
Igor Boyko <igor.boyko@hotelfriend.co>
2018-10-11 14:23:03 +0300

    admin menu

--- backend/controllers/SiteController.php
+++ backend/controllers/SiteController.php
@@ -43,7 +43,7 @@ class SiteController extends BaseController
                         'roles' => ['?'],
                     ],
                     [
-                        'actions' => ['*'],
+                        'actions' => ['index', 'error'],
                         'allow' => true,
                         'roles' => ['@'],
                     ],
