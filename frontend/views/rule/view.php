ebe63e5f849a8ad31e9f471f3ff929ab2326b06f
igosja <igosja@ukr.net>
2018-10-25 14:12:35 +0300

    rules

--- frontend/views/rule/view.php
+++ frontend/views/rule/view.php
@@ -1,5 +1,6 @@
 <?php
 
+use common\components\ErrorHelper;
 use yii\helpers\Html;
 
 /**
@@ -16,7 +17,15 @@ use yii\helpers\Html;
         </div>
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center text-size-3">
-                <?= Yii::$app->formatter->asDate($rule->rule_date); ?>
+                <?php
+
+                try {
+                    print Yii::$app->formatter->asDatetime($rule->rule_date, 'short');
+                } catch (Exception $e) {
+                    ErrorHelper::log($e);
+                }
+
+                ?>
             </div>
         </div>
         <div class="row">
@@ -26,7 +35,7 @@ use yii\helpers\Html;
         </div>
         <div class="row margin-top">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
-                <?= Html::a('Back', ['rule/index']); ?>
+                <?= Html::a('Назад', ['rule/index']); ?>
             </div>
         </div>
     </div>
