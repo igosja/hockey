0c1e9710d9585022c0dec2ab6b0dc59892d4d96c
Igor Boyko <igor.boyko@hotelfriend.co>
2018-10-31 17:14:17 +0200

    offseason

--- frontend/views/off-season/statistics.php
+++ frontend/views/off-season/statistics.php
@@ -20,7 +20,7 @@ use yii\helpers\Html;
 <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
         <h1>
-            Конференция любительских клубов
+            Кубок межсезонья
         </h1>
     </div>
 </div>
