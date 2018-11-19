1b1447f82add1a14b2764430cc486852f4001729
Igor Boyko <igor.boyko@hotelfriend.co>
2018-11-19 13:31:33 +0200

    country news page

--- frontend/views/country/_country.php
+++ frontend/views/country/_country.php
@@ -18,18 +18,6 @@ $file_name = 'file_name';
             </h1>
         </div>
     </div>
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
-            country_table_link
-        </div>
-    </div>
-<?php if ('country_national' == $file_name) : ?>
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
-            country_table_national_link.php
-        </div>
-    </div>
-<?php endif; ?>
     <div class="row margin-top">
         <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 text-right strong">
             Президент:
