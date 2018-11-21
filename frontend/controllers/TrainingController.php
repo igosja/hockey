e8c1a1a92f9ce0ab36eab031d94081f261fe603d
igosja <igosja@ukr.net>
2018-11-21 20:33:29 +0200

    training page

---
+++ frontend/controllers/TrainingController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace frontend\controllers;
+
+use common\models\Player;
+use common\models\Team;
+use Yii;
+use yii\data\ActiveDataProvider;
+
+/**
+ * Class TrainingController
+ * @package frontend\controllers
+ */
+class TrainingController extends AbstractController
+{
+    /**
+     * @return string|\yii\web\Response
+     * @throws \yii\web\ForbiddenHttpException
+     * @throws \yii\web\NotFoundHttpException
+     */
+    public function actionIndex()
+    {
+        if (Yii::$app->user->isGuest) {
+            $this->forbiddenAuth();
+        }
+
+        if (!$this->myTeam) {
+            return $this->redirect(['team/ask']);
+        }
+
+        $team = Team::find()
+            ->where(['team_id' => $this->myTeam->team_id])
+            ->limit(1)
+            ->one();
+        $this->notFound($team);
+
+        $query = Player::find()
+            ->where(['player_team_id' => $team->team_id, 'player_loan_team_id' => 0]);
+        $dataProvider = new ActiveDataProvider([
+            'pagination' => false,
+            'query' => $query,
+        ]);
+
+        $this->setSeoTitle('Тенировка хоккеистов');
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+            'team' => $team,
+        ]);
+    }
+}
