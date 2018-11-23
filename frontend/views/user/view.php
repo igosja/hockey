fe00d3fb554b8be5bb0210868e210b6b46e1e689
Igor Boyko <igor.boyko@hotelfriend.co>
2018-11-23 16:42:09 +0200

    user page

--- frontend/views/user/view.php
+++ frontend/views/user/view.php
@@ -2,39 +2,102 @@
 
 use common\components\ErrorHelper;
 use common\models\Country;
+use common\models\Team;
 use yii\grid\GridView;
 
 /**
  * @var \yii\data\ActiveDataProvider $countryDataProvider
+ * @var \yii\data\ActiveDataProvider $teamDataProvider
  */
 
 print $this->render('_top');
 
 ?>
 <div class="row">
-    <div class="row">
-        <?php
+    <?php
 
-        try {
-            $columns = [
-                [
-                    'label' => 'Роль в игре',
-                    'value' => function (Country $model): string {
-                        return $model->country_name . (Yii::$app->user->id == $model->country_president_id ? ' (Президент федерации)' : ' (Заместитель президента федерации)');
+    try {
+        $columns = [
+            [
+                'footer' => 'Роль в игре',
+                'label' => 'Роль в игре',
+                'value' => function (Country $model): string {
+                    $result = $model->country_name . ' ';
+                    if (Yii::$app->user->id == $model->country_president_id) {
+                        $result = $result . '(Президент федерации)';
+                    } else {
+                        $result = $result . '(Заместитель президента федерации)';
                     }
-                ],
-            ];
-            print GridView::widget([
-                'columns' => $columns,
-                'dataProvider' => $countryDataProvider,
-                'emptyText' => false,
-                'showFooter' => true,
-                'showOnEmpty' => false,
-            ]);
-        } catch (Exception $e) {
-            ErrorHelper::log($e);
-        }
+                    return $result;
+                }
+            ],
+        ];
+        print GridView::widget([
+            'columns' => $columns,
+            'dataProvider' => $countryDataProvider,
+            'emptyText' => false,
+            'showFooter' => true,
+            'showOnEmpty' => false,
+        ]);
+    } catch (Exception $e) {
+        ErrorHelper::log($e);
+    }
 
-        ?>
-    </div>
+    ?>
+</div>
+<div class="row">
+    <?php
+
+    try {
+        $columns = [
+            [
+                'footer' => 'Команда',
+                'label' => 'Команда',
+                'value' => function (Team $model): string {
+                    return $model->teamLink('img');
+                }
+            ],
+            [
+                'contentOptions' => ['class' => 'hidden-xs text-center'],
+                'footer' => 'Дивизион',
+                'footerOptions' => ['class' => 'hidden-xs'],
+                'headerOptions' => ['class' => 'col-20 hidden-xs'],
+                'label' => 'Дивизион',
+                'value' => function (Team $model): string {
+                    return $model->division();
+                }
+            ],
+            [
+                'contentOptions' => ['class' => 'text-center'],
+                'footer' => 'Vs',
+                'footerOptions' => ['title' => 'Рейтинг силы команды в длительных соревнованиях'],
+                'headerOptions' => ['class' => 'col-10', 'title' => 'Рейтинг силы команды в длительных соревнованиях'],
+                'label' => 'Vs',
+                'value' => function (Team $model): string {
+                    return $model->team_power_vs;
+                }
+            ],
+            [
+                'contentOptions' => ['class' => 'hidden-xs text-right'],
+                'footer' => 'Стоимость',
+                'footerOptions' => ['class' => 'hidden-xs'],
+                'headerOptions' => ['class' => 'col-20 hidden-xs'],
+                'label' => 'Стоимость',
+                'value' => function (Team $model): string {
+                    return Yii::$app->formatter->asCurrency($model->team_price_total, 'USD');
+                }
+            ],
+        ];
+        print GridView::widget([
+            'columns' => $columns,
+            'dataProvider' => $teamDataProvider,
+            'emptyText' => false,
+            'showFooter' => true,
+            'showOnEmpty' => false,
+        ]);
+    } catch (Exception $e) {
+        ErrorHelper::log($e);
+    }
+
+    ?>
 </div>
