095b8e3b4b8c7755c8d3cdd8a3f60e9246f3c392
Igor Boyko <igor.boyko@hotelfriend.co>
2018-11-26 14:49:48 +0200

    country poll page

--- frontend/views/country/poll.php
+++ frontend/views/country/poll.php
@@ -11,11 +11,6 @@ print $this->render('_country');
 
 ?>
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <h1>Опросы</h1>
-    </div>
-</div>
-<div class="row">
     <?php
 
     try {
