ba45ddb4110e3be4801969f1e9c24009f85f3673
Igor Boyko <igor.boyko@hotelfriend.co>
2018-11-28 16:59:24 +0200

    user questionnaire page

--- frontend/views/user/questionnaire.php
+++ frontend/views/user/questionnaire.php
@@ -62,30 +62,32 @@ print $this->render('_top');
 ); ?>
 <div class="row">
     <div class="col-lg-5 col-md-4 col-sm-4 col-xs-12 text-right xs-text-center">
-        <?= Html::label('Дата рождения'); ?>
+        <?= Html::label('Дата рождения', null, ['class' => 'strong']); ?>
     </div>
     <div class="col-lg-3 col-md-5 col-sm-5 col-xs-12">
-        <?= $form->field($model, 'user_birth_day', [
-            'options' => ['class' => 'row'],
-            'template' => '<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">{input}</div>',
-        ])->dropDownList(
-            $dayArray,
-            ['class' => 'form-control form-small', 'prompt' => '-']
-        ); ?>
-        <?= $form->field($model, 'user_birth_month', [
-            'options' => ['class' => 'row'],
-            'template' => '<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">{input}</div>',
-        ])->dropDownList(
-            $monthArray,
-            ['class' => 'form-control form-small', 'prompt' => '-']
-        ); ?>
-        <?= $form->field($model, 'user_birth_year', [
-            'options' => ['class' => 'row'],
-            'template' => '<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">{input}</div>',
-        ])->dropDownList(
-            $yearArray,
-            ['class' => 'form-control form-small', 'prompt' => '-']
-        ); ?>
+        <div class="row">
+            <?= $form->field($model, 'user_birth_day', [
+                'options' => ['class' => 'col-lg-4 col-md-4 col-sm-4 col-xs-12'],
+                'template' => '{input}',
+            ])->dropDownList(
+                $dayArray,
+                ['class' => 'form-control form-small', 'prompt' => '-']
+            ); ?>
+            <?= $form->field($model, 'user_birth_month', [
+                'options' => ['class' => 'col-lg-4 col-md-4 col-sm-4 col-xs-12'],
+                'template' => '{input}',
+            ])->dropDownList(
+                $monthArray,
+                ['class' => 'form-control form-small', 'prompt' => '-']
+            ); ?>
+            <?= $form->field($model, 'user_birth_year', [
+                'options' => ['class' => 'col-lg-4 col-md-4 col-sm-4 col-xs-12'],
+                'template' => '{input}',
+            ])->dropDownList(
+                $yearArray,
+                ['class' => 'form-control form-small', 'prompt' => '-']
+            ); ?>
+        </div>
     </div>
 </div>
 <div class="row">
