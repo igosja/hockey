aaade84755a4dbb61c1f790a58429c5d27bc7eea
Igor Boyko <igor.boyko@hotelfriend.co>
2018-11-29 15:54:23 +0200

    team logo page

--- common/models/Logo.php
+++ common/models/Logo.php
@@ -2,6 +2,10 @@
 
 namespace common\models;
 
+use common\components\HockeyHelper;
+use Yii;
+use yii\db\ActiveQuery;
+
 /**
  * Class Logo
  * @package common\models
@@ -11,6 +15,8 @@ namespace common\models;
  * @property int $logo_team_id
  * @property int $logo_text
  * @property int $logo_user_id
+ *
+ * @property Team $team
  */
 class Logo extends AbstractActiveRecord
 {
@@ -33,5 +39,29 @@ class Logo extends AbstractActiveRecord
             [['logo_text'], 'string', 'max' => 255],
             [['logo_text'], 'trim'],
         ];
+    }
+
+    /**
+     * @param bool $insert
+     * @return bool
+     */
+    public function beforeSave($insert): bool
+    {
+        if (parent::beforeSave($insert)) {
+            if ($this->isNewRecord) {
+                $this->logo_date = HockeyHelper::unixTimeStamp();
+                $this->logo_user_id = Yii::$app->user->id;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @return ActiveQuery
+     */
+    public function getTeam(): ActiveQuery
+    {
+        return $this->hasOne(Team::class, ['team_id' => 'logo_team_id']);
     }
 }
