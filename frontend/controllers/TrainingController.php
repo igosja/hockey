2e9c00f1d189c883a532438aa366c32dc2fc427d
Igor Boyko <igor.boyko@hotelfriend.co>
2018-12-17 12:33:45 +0200

    training page

--- frontend/controllers/TrainingController.php
+++ frontend/controllers/TrainingController.php
@@ -2,11 +2,19 @@
 
 namespace frontend\controllers;
 
+use common\components\ErrorHelper;
 use common\models\Building;
+use common\models\Finance;
+use common\models\FinanceText;
 use common\models\Loan;
 use common\models\Player;
+use common\models\PlayerPosition;
+use common\models\PlayerSpecial;
+use common\models\Position;
+use common\models\Special;
 use common\models\Training;
 use common\models\Transfer;
+use Exception;
 use Yii;
 use yii\data\ActiveDataProvider;
 use yii\filters\AccessControl;
@@ -70,6 +78,7 @@ class TrainingController extends AbstractController
 
     /**
      * @return string|\yii\web\Response
+     * @throws \yii\db\Exception
      */
     public function actionTrain()
     {
@@ -78,6 +87,11 @@ class TrainingController extends AbstractController
         }
 
         $team = $this->myTeam;
+
+        if ($team->buildingBase) {
+            $this->setErrorFlash('На базе сейчас идет строительство.');
+            return $this->redirect(['transfer/index']);
+        }
 
         $data = Yii::$app->request->get();
 
@@ -131,12 +145,247 @@ class TrainingController extends AbstractController
                 ];
 
                 $confirmData['price'] = $confirmData['price'] + $team->baseTraining->base_training_power_price;
+
+                $playerIdArray[] = $playerId;
             }
+        }
+
+        if (isset($data['position'])) {
+            foreach ($data['position'] as $playerId => $special) {
+                $player = Player::find()
+                    ->where(['player_id' => $playerId, 'player_team_id' => $team->team_id, 'player_loan_team_id' => 0])
+                    ->limit(1)
+                    ->one();
+                if (!$player) {
+                    $this->setErrorFlash('Нельзя тренировать игрока, который находиться в аренде.');
+                    return $this->redirect(['transfer/index']);
+                }
+
+                $transfer = Transfer::find()
+                    ->where(['transfer_player_id' => $playerId, 'transfer_ready' => 0])
+                    ->count();
+                if (!$transfer) {
+                    $this->setErrorFlash('Нельзя тренировать игрока, который выставлен на трансфер.');
+                    return $this->redirect(['transfer/index']);
+                }
+
+                $loan = Loan::find()
+                    ->where(['loan_player_id' => $playerId, 'loan_ready' => 0])
+                    ->count();
+                if (!$loan) {
+                    $this->setErrorFlash('Нельзя тренировать игрока, который выставлен на арендный рынок.');
+                    return $this->redirect(['transfer/index']);
+                }
+
+                $training = Training::find()
+                    ->where(['training_player_id' => $playerId, 'training_ready' => 0])
+                    ->count();
+                if ($training) {
+                    $this->setErrorFlash('Одному игроку нельзя назначить несколько тренировок одновременно.');
+                    return $this->redirect(['transfer/index']);
+                }
+
+                $special = Position::find()
+                    ->where(['position_id' => $special])
+                    ->andWhere([
+                        'not',
+                        [
+                            'position_id' => PlayerPosition::find()
+                                ->select(['player_position_position_id'])
+                                ->where(['player_position_player_id' => $playerId])
+                        ]
+                    ])
+                    ->limit(1)
+                    ->one();
+                if ($special) {
+                    $this->setErrorFlash('Совмещение выбрано не правильно.');
+                    return $this->redirect(['transfer/index']);
+                }
+
+                $confirmData['power'][] = [
+                    'id' => $playerId,
+                    'name' => $player->playerName(),
+                    'position' => [
+                        'id' => $special->position_id,
+                        'name' => $special->position_name,
+                    ],
+                ];
+
+                $confirmData['price'] = $confirmData['price'] + $team->baseTraining->base_training_position_price;
+
+                $playerIdArray[] = $playerId;
+            }
+        }
+
+        if (isset($data['special'])) {
+            foreach ($data['special'] as $playerId => $special) {
+                $player = Player::find()
+                    ->where(['player_id' => $playerId, 'player_team_id' => $team->team_id, 'player_loan_team_id' => 0])
+                    ->limit(1)
+                    ->one();
+                if (!$player) {
+                    $this->setErrorFlash('Нельзя тренировать игрока, который находиться в аренде.');
+                    return $this->redirect(['transfer/index']);
+                }
+
+                $transfer = Transfer::find()
+                    ->where(['transfer_player_id' => $playerId, 'transfer_ready' => 0])
+                    ->count();
+                if (!$transfer) {
+                    $this->setErrorFlash('Нельзя тренировать игрока, который выставлен на трансфер.');
+                    return $this->redirect(['transfer/index']);
+                }
+
+                $loan = Loan::find()
+                    ->where(['loan_player_id' => $playerId, 'loan_ready' => 0])
+                    ->count();
+                if (!$loan) {
+                    $this->setErrorFlash('Нельзя тренировать игрока, который выставлен на арендный рынок.');
+                    return $this->redirect(['transfer/index']);
+                }
+
+                $training = Training::find()
+                    ->where(['training_player_id' => $playerId, 'training_ready' => 0])
+                    ->count();
+                if ($training) {
+                    $this->setErrorFlash('Одному игроку нельзя назначить несколько тренировок одновременно.');
+                    return $this->redirect(['transfer/index']);
+                }
+
+                $isGk = Position::GK == $player->player_position_id ? 1 : null;
+                $isField = Position::GK == $player->player_position_id ? null : 1;
+
+                $special = Special::find()
+                    ->where(['special_id' => $special])
+                    ->andFilterWhere(['special_gk' => $isGk, 'special_field' => $isField])
+                    ->andWhere([
+                        'not',
+                        [
+                            'special_id' => PlayerSpecial::find()
+                                ->select(['player_special_special_id'])
+                                ->where([
+                                    'player_special_player_id' => $playerId,
+                                    'player_special_level' => Special::MAX_LEVEL,
+                                ])
+                        ]
+                    ])
+                    ->limit(1)
+                    ->one();
+                if ($special) {
+                    $this->setErrorFlash('Спецвозможность выбрано не правильно.');
+                    return $this->redirect(['transfer/index']);
+                }
+
+                $confirmData['power'][] = [
+                    'id' => $playerId,
+                    'name' => $player->playerName(),
+                    'special' => [
+                        'id' => $special->special_id,
+                        'name' => $special->special_name,
+                    ],
+                ];
+
+                $confirmData['price'] = $confirmData['price'] + $team->baseTraining->base_training_special_price;
+
+                $playerIdArray[] = $playerId;
+            }
+        }
+
+        if (count($confirmData['power']) > $team->availableTrainingPower()) {
+            $this->setErrorFlash('У вас недостаточно баллов для тренировки.');
+            return $this->redirect(['transfer/index']);
+        } elseif (count($confirmData['position']) > $team->availableTrainingPosition()) {
+            $this->setErrorFlash('У вас недостаточно совмещений для тренировки.');
+            return $this->redirect(['transfer/index']);
+        } elseif (count($confirmData['special']) > $team->availableTrainingSpecial()) {
+            $this->setErrorFlash('У вас недостаточно спецвозможностей для тренировки.');
+            return $this->redirect(['transfer/index']);
+        } elseif (count($playerIdArray) != count(array_unique($playerIdArray))) {
+            $this->setErrorFlash('Одному игроку нельзя назначить несколько тренировок одновременно.');
+            return $this->redirect(['transfer/index']);
+        } elseif ($confirmData['price'] > $team->team_finance) {
+            $this->setErrorFlash('У вас недостаточно денег для тренировки.');
+            return $this->redirect(['transfer/index']);
+        }
+
+        if (Yii::$app->request->get('ok')) {
+            $transaction = Yii::$app->db->beginTransaction();
+            try {
+                foreach ($confirmData['power'] as $playerId => $power) {
+                    $model = new Training();
+                    $model->training_player_id = $playerId;
+                    $model->training_power = 1;
+                    $model->training_season_id = $this->seasonId;
+                    $model->training_team_id = $team->team_id;
+                    $model->save();
+
+                    Finance::log([
+                        'finance_finance_text_id' => FinanceText::OUTCOME_TRAINING_POWER,
+                        'finance_team_id' => $team->team_id,
+                        'finance_value' => -$team->baseTraining->base_training_power_price,
+                        'finance_value_after' => $team->team_finance - $team->baseTraining->base_training_power_price,
+                        'finance_value_before' => $team->team_finance,
+                    ]);
+
+                    $team->team_finance = $team->team_finance - $team->baseTraining->base_training_power_price;
+                    $team->save(true, ['team_finance']);
+                }
+
+                foreach ($confirmData['position'] as $playerId => $position) {
+                    $model = new Training();
+                    $model->training_player_id = $playerId;
+                    $model->training_position_id = $position['id'];
+                    $model->training_season_id = $this->seasonId;
+                    $model->training_team_id = $team->team_id;
+                    $model->save();
+
+                    Finance::log([
+                        'finance_finance_text_id' => FinanceText::OUTCOME_TRAINING_POSITION,
+                        'finance_team_id' => $team->team_id,
+                        'finance_value' => -$team->baseTraining->base_training_position_price,
+                        'finance_value_after' => $team->team_finance - $team->baseTraining->base_training_position_price,
+                        'finance_value_before' => $team->team_finance,
+                    ]);
+
+                    $team->team_finance = $team->team_finance - $team->baseTraining->base_training_position_price;
+                    $team->save(true, ['team_finance']);
+                }
+
+                foreach ($confirmData['special'] as $playerId => $special) {
+                    $model = new Training();
+                    $model->training_player_id = $playerId;
+                    $model->training_season_id = $this->seasonId;
+                    $model->training_special_id = $special['id'];
+                    $model->training_team_id = $team->team_id;
+                    $model->save();
+
+                    Finance::log([
+                        'finance_finance_text_id' => FinanceText::OUTCOME_TRAINING_SPECIAL,
+                        'finance_team_id' => $team->team_id,
+                        'finance_value' => -$team->baseTraining->base_training_special_price,
+                        'finance_value_after' => $team->team_finance - $team->baseTraining->base_training_special_price,
+                        'finance_value_before' => $team->team_finance,
+                    ]);
+
+                    $team->team_finance = $team->team_finance - $team->baseTraining->base_training_special_price;
+                    $team->save(true, ['team_finance']);
+                }
+
+                $transaction->commit();
+
+                $this->setSuccessFlash('Тренировка успешно началась.');
+            } catch (Exception $e) {
+                $transaction->rollBack();
+                ErrorHelper::log($e);
+                $this->setErrorFlash();
+            }
+            return $this->redirect(['training/index']);
         }
 
         $this->setSeoTitle($team->fullName() . '. Тренировка хоккеистов');
 
         return $this->render('train', [
+            'confirmData' => $confirmData,
             'team' => $team,
         ]);
     }
---
+++ frontend/views/training/train.php
@@ -0,0 +1,115 @@
+<?php
+
+use common\components\FormatHelper;
+use common\models\Team;
+use yii\helpers\Html;
+
+/**
+ * @var array $confirmData
+ * @var Team $team
+ * @var \yii\web\View $this
+ */
+
+?>
+<div class="row margin-top">
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+        <?= $this->render('//team/_team-top-left', ['team' => $team]); ?>
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 text-right">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 strong text-size-1">
+                Тренировочный центр
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                Уровень:
+                <span class="strong"><?= $team->baseTraining->base_training_level; ?></span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                Скорость тренировки:
+                <span class="strong"><?= $team->baseTraining->base_training_training_speed_min; ?>%</span>
+                -
+                <span class="strong"><?= $team->baseTraining->base_training_training_speed_max; ?>%</span>
+                за тур
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                Осталось тренировок силы:
+                <span class="strong"><?= 0; ?></span>
+                из
+                <span class="strong"><?= $team->baseTraining->base_training_power_count; ?></span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                Осталось спецвозможностей:
+                <span class="strong"><?= 0; ?></span>
+                из
+                <span class="strong"><?= $team->baseTraining->base_training_special_count; ?></span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                Осталось совмещений:
+                <span class="strong"><?= 0; ?></span>
+                из
+                <span class="strong"><?= $team->baseTraining->base_training_position_count; ?></span>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row margin-top">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+        <span class="strong">Стоимость тренировок:</span>
+        Балл силы
+        <span class="strong">
+            <?= FormatHelper::asCurrency($team->baseTraining->base_training_power_price); ?>
+        </span>
+        Спецвозможность
+        <span class="strong">
+            <?= FormatHelper::asCurrency($team->baseTraining->base_training_special_price); ?>
+        </span>
+        Совмещение
+        <span class="strong">
+            <?= FormatHelper::asCurrency($team->baseTraining->base_training_position_price); ?>
+        </span>
+    </div>
+</div>
+<div class="row margin-top">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+        Здесь - <span class="strong">в тренировочном центре</span> -
+        вы можете назначить тренировки силы, спецвозможностей или совмещений своим игрокам:
+    </div>
+</div>
+<?= Html::beginForm(['training/train', 'ok' => 1], 'get'); ?>
+<div class="row margin-top">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+        Будут проведены следующие тренировки:
+        <ul>
+            <?php foreach ($confirmData['power'] as $item) : ?>
+                <li><?= $item['name']; ?> +1 балл силы</li>
+                <?= Html::hiddenInput('power[' . $item['id'] . ']', 1); ?>
+            <?php endforeach; ?>
+            <?php foreach ($confirmData['position'] as $item) : ?>
+                <li><?= $item['name']; ?> позиция <?= $item['position']['name']; ?></li>
+                <?= Html::hiddenInput('position[' . $item['id'] . ']', $item['position']['id']); ?>
+            <?php endforeach; ?>
+            <?php foreach ($confirmData['special'] as $item) : ?>
+                <li><?= $item['name']; ?> спецвозможность <?= $item['special']['name']; ?></li>
+                <?= Html::hiddenInput('special[' . $item['id'] . ']', $item['special']['id']); ?>
+            <?php endforeach; ?>
+        </ul>
+        Общая стоимость тренировок <span class="strong"><?= FormatHelper::asCurrency($confirmData['price']); ?></span>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+        <?= Html::submitButton('Начать тренировку', ['class' => 'btn margin']); ?>
+        <?= Html::a('Отказаться', ['training/index'], ['class' => 'btn margin']); ?>
+    </div>
+</div>
+<?= Html::endForm(); ?>
