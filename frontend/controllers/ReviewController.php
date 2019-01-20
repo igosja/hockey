ac4f05482761f4bb97f6876ce2b97437a41f4633
Igor Boyko <igosja@ukr.net>
2019-01-20 16:46:30 +0200

    review create

--- frontend/controllers/ReviewController.php
+++ frontend/controllers/ReviewController.php
@@ -8,6 +8,7 @@ use common\models\Division;
 use common\models\Game;
 use common\models\Schedule;
 use common\models\TournamentType;
+use frontend\models\CreateReview;
 use yii\filters\AccessControl;
 
 /**
@@ -38,11 +39,11 @@ class ReviewController extends AbstractController
      * @param $countryId
      * @param $divisionId
      * @param $scheduleId
-     * @param $seasonId
-     * @return string
+     * @return string|\yii\web\Response
+     * @throws \yii\db\Exception
      * @throws \yii\web\NotFoundHttpException
      */
-    public function actionCreate($countryId, $divisionId, $scheduleId, $seasonId)
+    public function actionCreate($countryId, $divisionId, $scheduleId)
     {
         $country = Country::find()
             ->where(['country_id' => $countryId])
@@ -66,12 +67,12 @@ class ReviewController extends AbstractController
             ->joinWith(['schedule'])
             ->where([
                 'schedule_id' => $scheduleId,
-                'schedule_season_id' => $seasonId,
+                'schedule_season_id' => $schedule->schedule_season_id,
                 'schedule_tournament_type_id' => TournamentType::CHAMPIONSHIP,
                 'game_home_team_id' => Championship::find()
                     ->select(['championship_team_id'])
                     ->where([
-                        'championship_season_id' => $seasonId,
+                        'championship_season_id' => $schedule->schedule_season_id,
                         'championship_country_id' => $countryId,
                         'championship_division_id' => $divisionId,
                     ])
@@ -79,12 +80,30 @@ class ReviewController extends AbstractController
             ->orderBy(['game_id' => SORT_ASC])
             ->all();
 
+        $model = new CreateReview([
+            'countryId' => $countryId,
+            'divisionId' => $divisionId,
+            'scheduleId' => $schedule->schedule_id,
+            'seasonId' => $schedule->schedule_season_id,
+            'stageId' => $schedule->schedule_stage_id,
+        ]);
+        if ($model->saveReview()) {
+            $this->setSuccessFlash();
+            return $this->redirect([
+                'championship/index',
+                'countryId' => $countryId,
+                'divisionId' => $divisionId,
+                'seasonId' => $schedule->schedule_season_id,
+            ]);
+        }
+
         $this->setSeoTitle('Создание обзора');
 
         return $this->render('create', [
             'country' => $country,
             'division' => $division,
             'gameArray' => $gameArray,
+            'model' => $model,
             'schedule' => $schedule,
         ]);
     }
--- frontend/views/review/create.php
+++ frontend/views/review/create.php
@@ -1,12 +1,15 @@
 <?php
 
+use coderlex\wysibb\WysiBB;
 use common\components\HockeyHelper;
 use yii\helpers\Html;
+use yii\widgets\ActiveForm;
 
 /**
  * @var \common\models\Country $country
  * @var \common\models\Division $division
  * @var \common\models\Game[] $gameArray
+ * @var \frontend\models\CreateReview $model
  * @var \common\models\Schedule $schedule
  */
 
@@ -19,16 +22,11 @@ use yii\helpers\Html;
     </div>
 </div>
 <div class="row">
-    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <h1>
-            <?= $country->country_name; ?>
-        </h1>
-    </div>
-</div>
-<div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
-        <?= $country->country_name; ?>, <?= $division->division_name; ?>, <?= $schedule->stage->stage_name; ?>
-        , <?= $schedule->schedule_season_id; ?> сезон
+        <?= $country->country_name; ?>,
+        <?= $division->division_name; ?>,
+        <?= $schedule->stage->stage_name; ?>,
+        <?= $schedule->schedule_season_id; ?> сезон
     </div>
 </div>
 <div class="row">
@@ -44,55 +42,51 @@ use yii\helpers\Html;
         </p>
     </div>
 </div>
-<form action="/review_create.php?<?= http_build_query(array_merge($_GET, array('edit' => 0))); ?>" method="POST">
-    <div class="row">
+<?php $form = ActiveForm::begin([
+    'fieldConfig' => [
+        'errorOptions' => [
+            'class' => 'col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center notification-error',
+            'tag' => 'div'
+        ],
+        'options' => ['class' => 'row'],
+        'template' =>
+            '<div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">{label}</div>
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">{input}</div>
+            </div>
+            <div class="row">{error}</div>',
+    ],
+]); ?>
+<?= $form->field($model, 'title')->textInput(['class' => 'form-control']); ?>
+<?= $form->field($model, 'text')->widget(WysiBB::class); ?>
+<?php foreach ($gameArray as $item) : ?>
+    <div class="row margin-top">
         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 table-responsive">
             <table class="table">
-                <?php foreach ($gameArray as $item) { ?>
-                    <tr>
-                        <td class="text-right col-45">
-                            <?= $item->teamHome->teamLink('string', true); ?>
-                            <?= HockeyHelper::formatAuto($item->game_home_auto); ?>
-                        </td>
-                        <td class="text-center col-10">
-                            <?= Html::a(
-                                $item->formatScore(),
-                                ['game/view', 'id' => $item->game_id]
-                            ); ?>
-                        </td>
-                        <td>
-                            <?= $item->teamGuest->teamLink('string', true); ?>
-                            <?= HockeyHelper::formatAuto($item->game_guest_auto); ?>
-                        </td>
-                    </tr>
-                <?php } ?>
+                <tr>
+                    <td class="text-right col-45">
+                        <?= $item->teamHome->teamLink('string', true); ?>
+                        <?= HockeyHelper::formatAuto($item->game_home_auto); ?>
+                    </td>
+                    <td class="text-center col-10">
+                        <?= Html::a(
+                            $item->formatScore(),
+                            ['game/view', 'id' => $item->game_id]
+                        ); ?>
+                    </td>
+                    <td>
+                        <?= $item->teamGuest->teamLink('string', true); ?>
+                        <?= HockeyHelper::formatAuto($item->game_guest_auto); ?>
+                    </td>
+                </tr>
             </table>
         </div>
     </div>
-    <div class="row">
-        <div class="col-lg-2 col-md-2 col-sm-3 col-xs-4 text-right">
-            <label for="title">Заголовок:</label>
-        </div>
-        <div class="col-lg-10 col-md-10 col-sm-9 col-xs-8">
-            <input class="form-control" id="title" name="data[title]"/>
-        </div>
+    <?= $form->field($model, 'gameText[' . $item->game_id . ']')->widget(WysiBB::class)->label(false); ?>
+<?php endforeach; ?>
+<div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
+        <?= Html::submitButton('Сохранить', ['class' => 'btn margin']); ?>
     </div>
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center title-error notification-error"></div>
-    </div>
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
-            <label class="hidden" for="text"></label>
-            <textarea class="form-control" id="text" name="data[text]" rows="20">
-            </textarea>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center text-error notification-error"></div>
-    </div>
-    <div class="row">
-        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 text-center">
-            <input class="btn margin" type="submit" value="Предварительный просмотр">
-        </div>
-    </div>
-</form>
+</div>
+<?php ActiveForm::end(); ?>
