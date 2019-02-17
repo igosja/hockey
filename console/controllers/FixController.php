9a05d555b788f8cd539652031f720e75885cdc47
igosja <igosja@ukr.net>
2019-02-17 12:17:40 +0200

    player statistic

--- console/controllers/FixController.php
+++ console/controllers/FixController.php
@@ -9,11 +9,13 @@ use common\models\FinanceText;
 use common\models\Game;
 use common\models\Lineup;
 use common\models\Position;
+use common\models\Season;
 use common\models\Stage;
 use common\models\StatisticPlayer;
 use common\models\Team;
 use common\models\TournamentType;
 use Exception;
+use yii\db\Expression;
 
 /**
  * Class FixController
@@ -57,7 +59,7 @@ class FixController extends AbstractController
     }
 
     /**
-     * @throws \yii\db\Exception
+     * @throws \Exception
      */
     public function actionStat()
     {
@@ -70,6 +72,7 @@ class FixController extends AbstractController
             /**
              * @var Game $game
              */
+            $this->stdout('Начинаем игру ' . $game->game_id . '...' . PHP_EOL);
             $lineupArray = [];
             foreach ($game->lineup as $lineup) {
                 if ($lineup->lineup_position_id == Position::GK && $lineup->lineup_line_id) {
@@ -143,19 +146,17 @@ class FixController extends AbstractController
             $scoreWinId = 0;
             $shootOutWinId = 0;
             for ($i = 0; $i < 100; $i++) {
+                if (isset($result['penalty']['home']['minute'][0]) && $result['penalty']['home']['minute'][0] + 2 == $i) {
+                    unset($result['penalty']['home']['minute'][0]);
+                    $result['penalty']['home']['total']--;
+                }
+                if (isset($result['penalty']['guest']['minute'][0]) && $result['penalty']['guest']['minute'][0] + 2 == $i) {
+                    unset($result['penalty']['guest']['minute'][0]);
+                    $result['penalty']['guest']['total']--;
+                }
+                $result['penalty']['home']['minute'] = array_values($result['penalty']['home']['minute']);
+                $result['penalty']['guest']['minute'] = array_values($result['penalty']['guest']['minute']);
                 foreach ($eventArray as $event) {
-                    if (isset($result['penalty']['home']['minute'][0]) && $result['penalty']['home']['minute'][0] + 2 == $event->event_minute) {
-                        unset($result['penalty']['home']['minute'][0]);
-                        $result['penalty']['home']['total']--;
-                    }
-                    if (isset($result['penalty']['guest']['minute'][0]) && $result['penalty']['guest']['minute'][0] + 2 == $event->event_minute) {
-                        unset($result['penalty']['guest']['minute'][0]);
-                        $result['penalty']['guest']['total']--;
-                    }
-
-                    $result['penalty']['home']['minute'] = array_values($result['penalty']['home']['minute']);
-                    $result['penalty']['guest']['minute'] = array_values($result['penalty']['guest']['minute']);
-
                     if ($event->event_minute != $i) {
                         continue;
                     }
@@ -192,20 +193,20 @@ class FixController extends AbstractController
                             $lineupArray[$event->event_player_assist_1_id]['assist']++;
 
                             if ($result['penalty'][$team]['total'] > $result['penalty'][$opponent]['total'] && $result['penalty'][$opponent]['total'] < 2) {
-                                $lineupArray[$event->event_player_score_id]['assist_short']++;
+                                $lineupArray[$event->event_player_assist_1_id]['assist_short']++;
                             }
                             if ($result['penalty'][$team]['total'] < $result['penalty'][$opponent]['total'] && $result['penalty'][$team]['total'] < 2) {
-                                $lineupArray[$event->event_player_score_id]['assist_power']++;
+                                $lineupArray[$event->event_player_assist_1_id]['assist_power']++;
                             }
                         }
                         if ($event->event_player_assist_2_id) {
                             $lineupArray[$event->event_player_assist_2_id]['assist']++;
 
                             if ($result['penalty'][$team]['total'] > $result['penalty'][$opponent]['total'] && $result['penalty'][$opponent]['total'] < 2) {
-                                $lineupArray[$event->event_player_score_id]['assist_short']++;
+                                $lineupArray[$event->event_player_assist_2_id]['assist_short']++;
                             }
                             if ($result['penalty'][$team]['total'] < $result['penalty'][$opponent]['total'] && $result['penalty'][$team]['total'] < 2) {
-                                $lineupArray[$event->event_player_score_id]['assist_power']++;
+                                $lineupArray[$event->event_player_assist_2_id]['assist_power']++;
                             }
                         }
 
@@ -273,7 +274,7 @@ class FixController extends AbstractController
                     $lineup['face_off'] = 16;
                     $lineup['face_off_win'] = 8;
                 }
-                $lineup[$playerId] = $lineup;
+                $lineupArray[$playerId] = $lineup;
             }
 
             $countryId = isset($game->teamHome->championship->championship_country_id) ? $game->teamHome->championship->championship_country_id : 0;
@@ -341,53 +342,69 @@ class FixController extends AbstractController
                     $model->statistic_player_point = $model->statistic_player_point + $lineup['point'];
                     $model->statistic_player_save = $model->statistic_player_save + $lineup['save'];
                     $model->statistic_player_shot_gk = $model->statistic_player_shot_gk + $lineup['shot_gk'];
-                    $model->statistic_player_shutout = $model->statistic_player_shutout + $lineup['shootout'];
+                    $model->statistic_player_shutout = $model->statistic_player_shutout + $lineup['shutout'];
+                    $model->statistic_player_win = $model->statistic_player_win + $lineup['win'];
+                    $model->save();
+                } else {
+                    $model = StatisticPlayer::find()->where([
+                        'statistic_player_championship_playoff' => $is_playoff,
+                        'statistic_player_country_id' => $countryId,
+                        'statistic_player_division_id' => $divisionId,
+                        'statistic_player_is_gk' => 0,
+                        'statistic_player_player_id' => $lineup['lineup_player_id'],
+                        'statistic_player_season_id' => $game->schedule->schedule_season_id,
+                        'statistic_player_team_id' => $lineup['lineup_team_id'],
+                        'statistic_player_tournament_type_id' => $game->schedule->schedule_tournament_type_id,
+                    ])->limit(1)->one();
+                    if (!$model) {
+                        $model = new StatisticPlayer();
+                        $model->statistic_player_championship_playoff = $is_playoff;
+                        $model->statistic_player_country_id = $countryId;
+                        $model->statistic_player_division_id = $divisionId;
+                        $model->statistic_player_is_gk = 0;
+                        $model->statistic_player_player_id = $lineup['lineup_player_id'];
+                        $model->statistic_player_season_id = $game->schedule->schedule_season_id;
+                        $model->statistic_player_team_id = $lineup['lineup_team_id'];
+                        $model->statistic_player_tournament_type_id = $game->schedule->schedule_tournament_type_id;
+                    }
+                    $model->statistic_player_assist = $model->statistic_player_assist + $lineup['assist'];
+                    $model->statistic_player_assist_power = $model->statistic_player_assist_power + $lineup['assist_power'];
+                    $model->statistic_player_assist_short = $model->statistic_player_assist_short + $lineup['assist_short'];
+                    $model->statistic_player_shootout_win = $model->statistic_player_shootout_win + $lineup['shootout_win'];
+                    $model->statistic_player_face_off = $model->statistic_player_face_off + $lineup['face_off'];
+                    $model->statistic_player_face_off_win = $model->statistic_player_face_off_win + $lineup['face_off_win'];
+                    $model->statistic_player_game = $model->statistic_player_game + 1;
+                    $model->statistic_player_loose = $model->statistic_player_loose + $lineup['loose'];
+                    $model->statistic_player_penalty = $model->statistic_player_penalty + $lineup['penalty'];
+                    $model->statistic_player_plus_minus = $model->statistic_player_plus_minus + $lineup['plus_minus'];
+                    $model->statistic_player_point = $model->statistic_player_point + $lineup['point'];
+                    $model->statistic_player_score = $model->statistic_player_score + $lineup['score'];
+                    $model->statistic_player_score_draw = $model->statistic_player_score_draw + $lineup['score_draw'];
+                    $model->statistic_player_score_power = $model->statistic_player_score_power + $lineup['score_power'];
+                    $model->statistic_player_score_short = $model->statistic_player_score_short + $lineup['score_short'];
+                    $model->statistic_player_score_win = $model->statistic_player_score_win + $lineup['score_win'];
+                    $model->statistic_player_shot = $model->statistic_player_shot + $lineup['shot'];
                     $model->statistic_player_win = $model->statistic_player_win + $lineup['win'];
                     $model->save();
                 }
-
-                $model = StatisticPlayer::find()->where([
-                    'statistic_player_championship_playoff' => $is_playoff,
-                    'statistic_player_country_id' => $countryId,
-                    'statistic_player_division_id' => $divisionId,
-                    'statistic_player_is_gk' => 0,
-                    'statistic_player_player_id' => $lineup['lineup_player_id'],
-                    'statistic_player_season_id' => $game->schedule->schedule_season_id,
-                    'statistic_player_team_id' => $lineup['lineup_team_id'],
-                    'statistic_player_tournament_type_id' => $game->schedule->schedule_tournament_type_id,
-                ])->limit(1)->one();
-                if (!$model) {
-                    $model = new StatisticPlayer();
-                    $model->statistic_player_championship_playoff = $is_playoff;
-                    $model->statistic_player_country_id = $countryId;
-                    $model->statistic_player_division_id = $divisionId;
-                    $model->statistic_player_is_gk = 0;
-                    $model->statistic_player_player_id = $lineup['lineup_player_id'];
-                    $model->statistic_player_season_id = $game->schedule->schedule_season_id;
-                    $model->statistic_player_team_id = $lineup['lineup_team_id'];
-                    $model->statistic_player_tournament_type_id = $game->schedule->schedule_tournament_type_id;
-                }
-                $model->statistic_player_assist = $model->statistic_player_assist + $lineup['assist'];
-                $model->statistic_player_assist_power = $model->statistic_player_assist_power + $lineup['assist_power'];
-                $model->statistic_player_assist_short = $model->statistic_player_assist_short + $lineup['assist_short'];
-                $model->statistic_player_shootout_win = $model->statistic_player_shootout_win + $lineup['shootout_win'];
-                $model->statistic_player_face_off = $model->statistic_player_face_off + $lineup['face_off'];
-                $model->statistic_player_face_off_win = $model->statistic_player_face_off_win + $lineup['face_off_win'];
-                $model->statistic_player_game = $model->statistic_player_game + 1;
-                $model->statistic_player_loose = $model->statistic_player_loose + $lineup['loose'];
-                $model->statistic_player_penalty = $model->statistic_player_penalty + $lineup * 2;
-                $model->statistic_player_plus_minus = $model->statistic_player_plus_minus + $lineup['plus_minus'];
-                $model->statistic_player_point = $model->statistic_player_point + $lineup['point'];
-                $model->statistic_player_score = $model->statistic_player_score + $lineup['score'];
-                $model->statistic_player_score_draw = $model->statistic_player_score_draw + $lineup['score_draw'];
-                $model->statistic_player_score_power = $model->statistic_player_score_power + $lineup['score_power'];
-                $model->statistic_player_score_short = $model->statistic_player_score_short + $lineup['score_short'];
-                $model->statistic_player_score_win = $model->statistic_player_score_win + $lineup['score_win'];
-                $model->statistic_player_shot = $model->statistic_player_shot + $lineup['shot'];
-                $model->statistic_player_win = $model->statistic_player_win + $lineup['win'];
-                $model->save();
             }
-            exit;
         }
+
+        StatisticPlayer::updateAll(
+            [
+                'statistic_player_pass_per_game' => new Expression('statistic_player_pass/IF(statistic_player_game=0,1,statistic_player_game)'),
+                'statistic_player_save_percent' => new Expression('statistic_player_save/IF(statistic_player_shot_gk=0,1,statistic_player_shot_gk)*100'),
+            ],
+            ['statistic_player_season_id' => Season::getCurrentSeason(), 'statistic_player_is_gk' => 1]
+        );
+
+        StatisticPlayer::updateAll(
+            [
+                'statistic_player_face_off_percent' => new Expression('statistic_player_face_off_win/IF(statistic_player_face_off=0,1,statistic_player_face_off)*100'),
+                'statistic_player_score_shot_percent' => new Expression('statistic_player_score/IF(statistic_player_shot=0,1,statistic_player_shot)*100'),
+                'statistic_player_shot_per_game' => new Expression('statistic_player_shot/IF(statistic_player_game=0,1,statistic_player_game)'),
+            ],
+            ['statistic_player_season_id' => Season::getCurrentSeason(), 'statistic_player_is_gk' => 0]
+        );
     }
 }
