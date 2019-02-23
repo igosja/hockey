27765b4359bb98a205122fd8255c8b9c14de99f7
igosja <igosja@ukr.net>
2019-02-23 15:13:03 +0200

    fix champ

--- console/models/generator/ChampionshipLot.php
+++ console/models/generator/ChampionshipLot.php
@@ -155,74 +155,74 @@ class ChampionshipLot
                         }
                     }
                 }
-            } elseif (Stage::SEMI == $schedule->schedule_stage_id) {
-                $check = Schedule::find()
-                    ->where(['schedule_tournament_type_id' => TournamentType::CHAMPIONSHIP])
-                    ->andWhere(['>', 'schedule_id', $schedule->schedule_id])
+            }
+        } elseif (Stage::SEMI == $schedule->schedule_stage_id) {
+            $check = Schedule::find()
+                ->where(['schedule_tournament_type_id' => TournamentType::CHAMPIONSHIP])
+                ->andWhere(['>', 'schedule_id', $schedule->schedule_id])
+                ->orderBy(['schedule_id' => SORT_ASC])
+                ->limit(1)
+                ->one();
+            if (Stage::FINAL_GAME == $check->schedule_stage_id) {
+                $stageArray = Schedule::find()
+                    ->where([
+                        'schedule_season_id' => $seasonId,
+                        'schedule_stage_id' => Stage::FINAL_GAME,
+                        'schedule_tournament_type_id' => TournamentType::CHAMPIONSHIP,
+                    ])
                     ->orderBy(['schedule_id' => SORT_ASC])
-                    ->limit(1)
-                    ->one();
-                if (Stage::FINAL_GAME == $check->schedule_stage_id) {
-                    $stageArray = Schedule::find()
-                        ->where([
-                            'schedule_season_id' => $seasonId,
-                            'schedule_stage_id' => Stage::FINAL_GAME,
-                            'schedule_tournament_type_id' => TournamentType::CHAMPIONSHIP,
-                        ])
-                        ->orderBy(['schedule_id' => SORT_ASC])
-                        ->limit(3)
-                        ->all();
+                    ->limit(3)
+                    ->all();
 
-                    $countryArray = ParticipantChampionship::find()
-                        ->where(['participant_championship_season_id' => $seasonId])
-                        ->groupBy(['participant_championship_country_id'])
-                        ->orderBy(['participant_championship_country_id' => SORT_ASC])
+                $countryArray = ParticipantChampionship::find()
+                    ->where(['participant_championship_season_id' => $seasonId])
+                    ->groupBy(['participant_championship_country_id'])
+                    ->orderBy(['participant_championship_country_id' => SORT_ASC])
+                    ->all();
+                foreach ($countryArray as $country) {
+                    $divisionArray = ParticipantChampionship::find()
+                        ->where([
+                            'participant_championship_season_id' => $seasonId,
+                            'participant_championship_country_id' => $country->participant_championship_country_id,
+                        ])
+                        ->groupBy(['participant_championship_division_id'])
+                        ->orderBy(['participant_championship_division_id' => SORT_ASC])
                         ->all();
-                    foreach ($countryArray as $country) {
-                        $divisionArray = ParticipantChampionship::find()
+                    foreach ($divisionArray as $division) {
+                        $teamArray = ParticipantChampionship::find()
+                            ->joinWith(['championship'])
+                            ->with(['championship.team'])
                             ->where([
                                 'participant_championship_season_id' => $seasonId,
                                 'participant_championship_country_id' => $country->participant_championship_country_id,
+                                'participant_championship_division_id' => $division->participant_championship_division_id,
+                                'participant_championship_stage_1' => 1,
+                                'participant_championship_stage_id' => 0,
                             ])
-                            ->groupBy(['participant_championship_division_id'])
-                            ->orderBy(['participant_championship_division_id' => SORT_ASC])
+                            ->orderBy(['championship_place' => SORT_ASC])
+                            ->limit(2)
                             ->all();
-                        foreach ($divisionArray as $division) {
-                            $teamArray = ParticipantChampionship::find()
-                                ->joinWith(['championship'])
-                                ->with(['championship.team'])
-                                ->where([
-                                    'participant_championship_season_id' => $seasonId,
-                                    'participant_championship_country_id' => $country->participant_championship_country_id,
-                                    'participant_championship_division_id' => $division->participant_championship_division_id,
-                                    'participant_championship_stage_1' => 1,
-                                    'participant_championship_stage_id' => 0,
-                                ])
-                                ->orderBy(['championship_place' => SORT_ASC])
-                                ->limit(2)
-                                ->all();
 
-                            $model = new Game();
-                            $model->game_guest_team_id = $teamArray[1]->participant_championship_team_id;
-                            $model->game_home_team_id = $teamArray[0]->participant_championship_team_id;
-                            $model->game_schedule_id = $stageArray[0]->schedule_id;
-                            $model->game_stadium_id = $teamArray[0]->championship->team->team_stadium_id;
-                            $model->save();
+                        $model = new Game();
+                        $model->game_guest_team_id = $teamArray[1]->participant_championship_team_id;
+                        $model->game_home_team_id = $teamArray[0]->participant_championship_team_id;
+                        $model->game_schedule_id = $stageArray[0]->schedule_id;
+                        $model->game_stadium_id = $teamArray[0]->championship->team->team_stadium_id;
+                        $model->save();
 
-                            $model = new Game();
-                            $model->game_guest_team_id = $teamArray[0]->participant_championship_team_id;
-                            $model->game_home_team_id = $teamArray[1]->participant_championship_team_id;
-                            $model->game_schedule_id = $stageArray[1]->schedule_id;
-                            $model->game_stadium_id = $teamArray[1]->championship->team->team_stadium_id;
-                            $model->save();
+                        $model = new Game();
+                        $model->game_guest_team_id = $teamArray[0]->participant_championship_team_id;
+                        $model->game_home_team_id = $teamArray[1]->participant_championship_team_id;
+                        $model->game_schedule_id = $stageArray[1]->schedule_id;
+                        $model->game_stadium_id = $teamArray[1]->championship->team->team_stadium_id;
+                        $model->save();
 
-                            $model = new Game();
-                            $model->game_guest_team_id = $teamArray[1]->participant_championship_team_id;
-                            $model->game_home_team_id = $teamArray[0]->participant_championship_team_id;
-                            $model->game_schedule_id = $stageArray[2]->schedule_id;
-                            $model->game_stadium_id = $teamArray[0]->championship->team->team_stadium_id;
-                            $model->save();
-                        }
+                        $model = new Game();
+                        $model->game_guest_team_id = $teamArray[1]->participant_championship_team_id;
+                        $model->game_home_team_id = $teamArray[0]->participant_championship_team_id;
+                        $model->game_schedule_id = $stageArray[2]->schedule_id;
+                        $model->game_stadium_id = $teamArray[0]->championship->team->team_stadium_id;
+                        $model->save();
                     }
                 }
             }
