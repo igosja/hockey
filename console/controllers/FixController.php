ac7229bc1eb4457979532c21200a0f7018028a31
igosja <igosja@ukr.net>
2019-02-28 22:15:40 +0200

    player ava

--- console/controllers/FixController.php
+++ console/controllers/FixController.php
@@ -14,7 +14,6 @@ use common\models\Stage;
 use common\models\StatisticPlayer;
 use common\models\Team;
 use common\models\TournamentType;
-use console\models\generator\ChampionshipLot;
 use Exception;
 use yii\db\Expression;
 
@@ -407,10 +406,5 @@ class FixController extends AbstractController
             ],
             ['statistic_player_season_id' => Season::getCurrentSeason(), 'statistic_player_is_gk' => 0]
         );
-    }
-
-    public function actionChamp()
-    {
-        (new ChampionshipLot())->execute();
     }
 }
