6ebb67ffd2553e51e9bcec869a428a1c73ea7a1a
Igor Boyko <igor.boyko@hotelfriend.co>
2019-07-29 15:14:08 +0300

    bot school

--- console/models/BotService.php
+++ console/models/BotService.php
@@ -2,13 +2,24 @@
 
 namespace console\models;
 
+use common\components\ErrorHelper;
+use common\models\BaseSchool;
+use common\models\Building;
+use common\models\BuildingBase;
+use common\models\ConstructionType;
+use common\models\Finance;
+use common\models\FinanceText;
 use common\models\Game;
 use common\models\Lineup;
 use common\models\Player;
 use common\models\Position;
+use common\models\School;
+use common\models\Season;
 use common\models\Site;
+use common\models\Style;
 use common\models\Team;
 use Exception;
+use Yii;
 
 /**
  * Class BotService
@@ -16,6 +27,9 @@ use Exception;
  */
 class BotService
 {
+    const COUNT_LINEUP = 22;
+    const SCHOOL_LEVEL = 1;
+
     /**
      * @throws Exception
      */
@@ -25,12 +39,15 @@ class BotService
             return;
         }
         $this->lineup();
+        $this->buildSchool();
+        $this->startSchool();
     }
 
     /**
+     * @return bool
      * @throws Exception
      */
-    private function lineup()
+    private function lineup(): bool
     {
         $team = Team::find()
             ->where(['team_user_id' => 0])
@@ -38,6 +55,10 @@ class BotService
             ->orderBy('RAND()')
             ->limit(1)
             ->one();
+        if (!$team) {
+            return true;
+        }
+
         $game = Game::find()
             ->joinWith(['schedule'], false)
             ->where([
@@ -50,7 +71,7 @@ class BotService
             ->limit(1)
             ->one();
         if (!$game) {
-            return;
+            return true;
         }
 
         $countLineup = Lineup::find()
@@ -60,12 +81,12 @@ class BotService
             ])
             ->count();
 
-        if ($game->game_home_team_id == $team->team_id && 22 == $countLineup) {
-            return;
+        if ($game->game_home_team_id == $team->team_id && self::COUNT_LINEUP == $countLineup) {
+            return true;
         }
 
-        if ($game->game_guest_team_id == $team->team_id && 22 == $countLineup) {
-            return;
+        if ($game->game_guest_team_id == $team->team_id && self::COUNT_LINEUP == $countLineup) {
+            return true;
         }
 
         Lineup::deleteAll(['lineup_game_id' => $game->game_id, 'lineup_team_id' => $team->team_id]);
@@ -128,7 +149,158 @@ class BotService
                 'lineup_player_id',
             ]);
         }
+
+        return true;
     }
 
+    /**
+     * @return bool
+     * @throws \yii\db\Exception
+     */
+    private function buildSchool(): bool
+    {
+        $team = Team::find()
+            ->where(['team_user_id' => 0, 'team_base_school_id' => 0])
+            ->andWhere(['!=', 'team_id', 0])
+            ->orderBy('RAND()')
+            ->limit(1)
+            ->one();
+        if (!$team) {
+            return true;
+        }
 
+        if ($team->buildingBase) {
+            return true;
+        }
+
+        if ($team->isSchool()) {
+            return true;
+        }
+
+        if ($team->base->base_slot_max <= $team->baseUsed()) {
+            return true;
+        }
+
+        $baseSchool = BaseSchool::find()
+            ->where(['base_school_level' => self::SCHOOL_LEVEL])
+            ->limit(1)
+            ->one();
+
+        if (!$baseSchool) {
+            return true;
+        }
+
+        if ($baseSchool->base_school_base_level > $team->base->base_level) {
+            return true;
+        }
+
+        if ($baseSchool->base_school_price_buy > $team->team_finance) {
+            return true;
+        }
+
+        $transaction = Yii::$app->db->beginTransaction();
+        try {
+            $model = new BuildingBase();
+            $model->building_base_building_id = Building::SCHOOL;
+            $model->building_base_construction_type_id = ConstructionType::BUILD;
+            $model->building_base_day = $baseSchool->base_school_build_speed;
+            $model->building_base_team_id = $team->team_id;
+            $model->save();
+
+            Finance::log([
+                'finance_building_id' => Building::SCHOOL,
+                'finance_finance_text_id' => FinanceText::OUTCOME_BUILDING_BASE,
+                'finance_level' => self::SCHOOL_LEVEL,
+                'finance_team_id' => $team->team_id,
+                'finance_value' => -$baseSchool->base_school_price_buy,
+                'finance_value_after' => $team->team_finance - $baseSchool->base_school_price_buy,
+                'finance_value_before' => $team->team_finance,
+            ]);
+
+            $team->team_finance = $team->team_finance - $baseSchool->base_school_price_buy;
+            $team->save(true, ['team_finance']);
+            $transaction->commit();
+        } catch (Exception $e) {
+            $transaction->rollBack();
+            ErrorHelper::log($e);
+        }
+
+        return true;
+    }
+
+    /**
+     * @return bool
+     * @throws \yii\db\Exception
+     */
+    public function startSchool(): bool
+    {
+        $team = Team::find()
+            ->where(['team_user_id' => 0])
+            ->andWhere(['!=', 'team_id', 0])
+            ->andWhere(['!=', 'team_base_school_id', 0])
+            ->orderBy('RAND()')
+            ->limit(1)
+            ->one();
+        if (!$team) {
+            return true;
+        }
+
+        if ($team->buildingBase) {
+            return true;
+        }
+
+        if (!$team->availableSchool()) {
+            return true;
+        }
+
+        $school = School::find()
+            ->where(['school_ready' => 0, 'school_team_id' => $team->team_id])
+            ->count();
+        if ($school) {
+            return true;
+        }
+
+        for ($position= Position::GK; $position<=Position::RW; $position++) {
+            $minCount = 5;
+            if (Position::GK == $position) {
+                $minCount = 2;
+            }
+
+            $countPlayer = Player::find()
+                ->where(['player_team_id' => $team->team_id, 'player_position_id' => $position])
+                ->count();
+            if ($minCount > $countPlayer) {
+                $style = Style::find()
+                    ->andWhere(['!=', 'style_id', Style::NORMAL])
+                    ->orderBy('RAND()')
+                    ->limit(1)
+                    ->one();
+
+                $transaction = Yii::$app->db->beginTransaction();
+                try {
+                    $model = new School();
+                    $model->school_day = $team->baseSchool->base_school_school_speed;
+                    $model->school_position_id = $position;
+                    $model->school_season_id = Season::getCurrentSeason();
+                    $model->school_special_id = null;
+                    $model->school_style_id = $style->style_id;
+                    $model->school_team_id = $team->team_id;
+                    $model->school_with_special = 0;
+                    $model->school_with_special_request = 0;
+                    $model->school_with_style = 0;
+                    $model->school_with_style_request = 0;
+                    $model->save();
+
+                    $transaction->commit();
+                } catch (Exception $e) {
+                    $transaction->rollBack();
+                    ErrorHelper::log($e);
+                }
+
+                return true;
+            }
+        }
+
+        return true;
+    }
 }
